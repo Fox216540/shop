9a700d4971131239f2eed2155bd90b212b034d21
Andrey <snick25rs@gmail.com>
2025-07-12 21:31:08 +0200

    Улучшил подключение к бд

--- src/infra/db/database.go
+++ src/infra/db/database.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
@@ -16,12 +17,22 @@ var database *Database
 
 // Init инициализирует глобальный объект Database
 func Init() {
-	dsn := os.Getenv("DATABASE_URL")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	databaseName := os.Getenv("POSTGRES_DB")
+	fmt.Println(user, password, host, port, databaseName)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, databaseName, port,
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
 	database = &Database{DB: db}
+	err = database.DB.AutoMigrate(&User{}, &Product{}, &Order{})
 }
 
 // Close завершает подключение к базе
@@ -35,4 +46,8 @@ func Close() {
 		return
 	}
 	sqlDB.Close()
+}
+
+func GetDatabase() *Database {
+	return database
 }
