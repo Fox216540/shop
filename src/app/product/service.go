b50c967a9b52002803acc2c44722f37c889964cf
Andrey <snick25rs@gmail.com>
2025-07-12 03:44:54 +0200

    Добавил реализацию сервиса для управления продуктами, включая методы получения продуктов по категории и по идентификатору

---
+++ src/app/product/service.go
@@ -0,0 +1,30 @@
+package productservice
+
+import (
+	"github.com/google/uuid"
+	"shop/src/domain/product"
+)
+
+type service struct {
+	r product.Repository
+}
+
+func NewService(r product.Repository) product.Service {
+	return &service{r: r}
+}
+
+func (s *service) ProductsOfCategory(c string) ([]product.Product, error) {
+	products, err := s.r.FindProductsByCategory(c)
+	if err != nil {
+		return nil, err // Возвращаем ошибку, если не удалось найти продукты
+	}
+	return products, nil
+}
+
+func (s *service) ProductById(id uuid.UUID) (product.Product, error) {
+	p, err := s.r.FindProductByID(id)
+	if err != nil {
+		return p, err // Возвращаем ошибку, если не удалось найти продукт
+	}
+	return p, nil
+}
