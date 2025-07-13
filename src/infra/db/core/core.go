f93a1f034fe0b121d5a639ae4001d8259917318f
Andrey <snick25rs@gmail.com>
2025-07-13 03:25:32 +0200

    Добавил ядро бд

---
+++ src/infra/db/core/core.go
@@ -0,0 +1,25 @@
+package dbcore
+
+import "gorm.io/gorm"
+
+type Database struct {
+	DB *gorm.DB
+}
+
+// Глобальный объект Database
+var database *Database
+
+func InitDatabase(db *gorm.DB) {
+	if db == nil {
+		panic("InitDatabase: db is nil")
+	}
+	database = &Database{DB: db}
+}
+
+func GetDatabase() *Database {
+	return database
+}
+
+func (d *Database) WithSession(fn func(tx *gorm.DB) error) error {
+	return d.DB.Transaction(fn)
+}
