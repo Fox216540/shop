f7db13983c5b64e8ddc67998175e5bafdf33c376
Andrey <snick25rs@gmail.com>
2025-07-13 21:20:08 +0200

    Добавил создание суммы и номера заказа

--- src/app/order/service.go
+++ src/app/order/service.go
@@ -1,8 +1,11 @@
 package orderservice
 
 import (
+	"fmt"
 	"github.com/google/uuid"
+	"math/rand"
 	"shop/src/domain/order"
+	"time"
 )
 
 type service struct {
@@ -17,11 +20,16 @@ func (s *service) PlaceOrder(o order.Order) (order.Order, error) {
 	if o.ID == uuid.Nil {
 		o.ID = uuid.New()
 	}
-	orderSave, err := s.r.Save(o)
+
+	o.OrderNum = s.GenerateOrderNum() // Генерируем уникальный номер заказа
+
+	o.Total = s.CalculateTotal(o) // Вычисляем общую сумму заказа
+
+	savedOrder, err := s.r.Save(o)
 	if err != nil {
 		return o, err // Возвращаем ошибку, если не удалось сохранить заказ
 	}
-	return orderSave, nil
+	return savedOrder, nil
 }
 
 func (s *service) CancelOrder(ID, userID uuid.UUID) (uuid.UUID, error) {
@@ -45,4 +53,35 @@ func (s *service) OrdersByUserID(userID uuid.UUID) ([]order.Order, error) {
 		return nil, err // Возвращаем ошибку, если не удалось найти заказы пользователя
 	}
 	return orders, nil
+}
+
+func (s *service) CalculateTotal(order order.Order) float64 {
+	var total float64
+	if len(order.ProductItems) == 0 {
+		return 0 // Возвращаем 0, если нет товаров в заказе
+	}
+	for _, item := range order.ProductItems {
+		total += item.Product.Price * float64(item.Quantity)
+	}
+	return total
+}
+
+func (s *service) GenerateOrderNum() string {
+	// Генерируем уникальный номер заказа на основе текущей даты и случайного числа
+	datetime := time.Now().Format("20060102")
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+
+	var orderNum string
+
+	for {
+		randomPart := r.Intn(10000)                                 // Генерируем новый случайный номер
+		orderNum = fmt.Sprintf("ORD-%s-%04d", datetime, randomPart) // Форматируем новый номер заказа
+		err := s.r.CheckOrderNum(orderNum)                          // Проверяем, существует ли уже такой номер заказа
+		if err == nil {
+			break // Если номер заказа уникален, выходим из цикла
+		}
+	}
+
+	return orderNum
 }
