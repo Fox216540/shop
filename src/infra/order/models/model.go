09ab8b8f680d5cba1eab312b334033ed457ebdc3
Andrey <snick25rs@gmail.com>
2025-07-13 16:20:31 +0200

    Добавил новые связи

--- src/infra/order/models/model.go
+++ src/infra/order/models/model.go
@@ -2,37 +2,46 @@ package ordermodels
 
 import (
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 	"shop/src/domain/order"
-	"shop/src/domain/product"
+	productORM "shop/src/infra/product/models"
+	user "shop/src/infra/user/models"
 )
 
 type ProductItemORM struct {
+	gorm.Model
 	ID        int       `gorm:"primaryKey;autoIncrement"`
 	OrderID   uuid.UUID `gorm:"type:uuid;not null;index"`
 	ProductID uuid.UUID `gorm:"type:uuid;not null;index"`
+
+	Product productORM.ProductORM `gorm:"references:ProductID"`
 
 	Quantity int `gorm:"not null"`
 }
 
 type OrderORM struct {
+	gorm.Model
 	ID           int               `gorm:"primaryKey;autoIncrement"`
 	OrderID      uuid.UUID         `gorm:"type:uuid;not null;uniqueIndex"`
+	UserID       uuid.UUID         `gorm:"type:uuid;not null;index"`
 	OrderNum     string            `gorm:"type:varchar(50);not null;uniqueIndex"`
 	Status       string            `gorm:"type:varchar(50);not null"`
 	ProductItems []*ProductItemORM `gorm:"foreignKey:OrderID;references:OrderID;constraint:OnDelete:CASCADE"`
+
+	User user.UserORM `gorm:"references:UserID"`
 }
 
-func FromORM(orm OrderORM, productMap map[uuid.UUID]product.Product) order.Order {
+func FromORM(orm OrderORM) order.Order {
 	o := order.Order{
 		ID:           orm.OrderID,
 		OrderNum:     orm.OrderNum,
 		Status:       orm.Status,
+		UserId:       orm.UserID,
 		ProductItems: make([]*order.Item, 0, len(orm.ProductItems)),
 	}
 	for _, item := range orm.ProductItems {
-		p := productMap[item.ProductID]
 		o.ProductItems = append(o.ProductItems, &order.Item{
-			Product:  p,
+			Product:  productORM.FromORM(item.Product),
 			Quantity: item.Quantity,
 		})
 	}
--- src/infra/product/models/model.go
+++ src/infra/product/models/model.go
@@ -2,11 +2,12 @@ package productmodel
 
 import (
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 	"shop/src/domain/product"
-	ordermodels "shop/src/infra/order/models"
 )
 
 type ProductORM struct {
+	gorm.Model
 	ID          int       `gorm:"primaryKey;autoIncrement"`
 	ProductID   uuid.UUID `gorm:"type:uuid;not null;uniqueIndex"`
 	Name        string    `gorm:"not null;unique"`             // Unique product name
@@ -15,8 +16,6 @@ type ProductORM struct {
 	Category    string    `gorm:"not null"`                    // Product category
 	Description string    `gorm:"not null"`                    // Product description
 	Stock       int       `gorm:"not null"`                    // Product stock quantity
-
-	ProductItems []*ordermodels.ProductItemORM `gorm:"foreignKey:ProductID;references:ProductID;constraint:OnDelete:CASCADE"`
 }
 
 func FromORM(orm ProductORM) product.Product {
