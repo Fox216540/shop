b0a9bd629ce87949c7afa45187f003d76787f371
Andrey <snick25rs@gmail.com>
2025-07-13 03:32:01 +0200

    Улучшил запуск

--- start.go
+++ start.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
 	"shop/src/api/product"
-	"shop/src/api/user"
+	"shop/src/infra/db"
 )
 
 func main() {
+	err := godotenv.Load() // по умолчанию ищет файл .env в текущей папке
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	db.Init()
+	defer db.Close()
 	r := gin.Default()
-	catalog_handler.CatalogHandler(r)
-	user_handler.UserHandler(r)
+	producthandler.CatalogHandler(r)
 	r.Run(":8080") // Start the server on port 8080
 }
