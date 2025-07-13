e406e783bd6ed6cf3996197650cbee415f4fd5b8
Andrey <snick25rs@gmail.com>
2025-07-13 19:28:05 +0200

    Добавил handler для product

--- src/api/product/handler.go
+++ src/api/product/handler.go
@@ -2,20 +2,49 @@ package producthandler
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 	di "shop/src/api/product/di"
 	dto "shop/src/api/product/dto"
 )
 
 func CatalogHandler(r *gin.Engine) {
-	r.GET("/catalog", func(c *gin.Context) {
-		// Simulate fetching product data
+	r.GET("/catalog/", func(c *gin.Context) {
+		category := c.Query("category")
+		var categoryPtr *string
+		if category != "" {
+			categoryPtr = &category
+		} else {
+			categoryPtr = nil
+		}
 		ps := di.GetProductService()
-		products, err := ps.ProductsOfCategory("electronics")
+		products, err := ps.ProductsOfCategory(categoryPtr)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch product data"})
 			return
 		}
 		c.JSON(http.StatusOK, dto.GetProductsByCategoryResponse{Products: products})
+	})
+	r.GET("/catalog/:id", func(c *gin.Context) {
+		productID, err := uuid.Parse(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid product ID"})
+			return
+		}
+		ps := di.GetProductService()
+		product, err := ps.ProductById(productID)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Product not found"})
+			return
+		}
+		c.JSON(http.StatusOK, dto.GetProductByIDResponse{
+			ProductID:          product.ID,
+			ProductName:        product.Name,
+			ProductImg:         product.Img,
+			ProductPrice:       product.Price,
+			ProductCategory:    product.Category,
+			ProductDescription: product.Description,
+			ProductStock:       product.Stock,
+		})
 	})
 }
