7a81be64c748c1f5533bd13b59612fbd04e9cfff
Andrey <snick25rs@gmail.com>
2025-07-14 01:25:46 +0200

    Перенес usecase interface в app

--- src/api/product/di/di.go
+++ src/api/product/di/di.go
@@ -2,12 +2,11 @@ package productdi
 
 import (
 	productservice "shop/src/app/product"
-	"shop/src/domain/product"
 	db "shop/src/infra/db/core"
 	productrepository "shop/src/infra/product"
 )
 
-func GetProductService() product.Service {
+func GetProductService() productservice.Service {
 	database := db.GetDatabase() // Получаем базу данных из пакета db
 	repo := productrepository.NewRepository(database)
 	return productservice.NewService(repo)
