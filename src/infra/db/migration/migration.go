55d7a64b8f98b89183f16af8a6035ab6522c8ee7
Andrey <snick25rs@gmail.com>
2025-07-13 18:44:44 +0200

    Добавил нужное расширение

--- src/infra/db/migration/migration.go
+++ src/infra/db/migration/migration.go
@@ -1,6 +1,7 @@
 package migration
 
 import (
+	"fmt"
 	"gorm.io/gorm"
 	order "shop/src/infra/order/models"
 	product "shop/src/infra/product/models"
@@ -8,6 +9,10 @@ import (
 )
 
 func Migration(db *gorm.DB) error {
+	if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "pgcrypto"`).Error; err != nil {
+		return fmt.Errorf("failed to create extension pgcrypto: %w", err)
+	}
+
 	return db.AutoMigrate(
 		&product.ProductORM{},
 		&order.OrderORM{},
