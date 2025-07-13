456fc6b2f45d18a85316404e50c8b0e185a8e2bf
Andrey <snick25rs@gmail.com>
2025-07-13 21:20:41 +0200

    Добавил функции в service order: CalculateTotal, GenerateOrderNum

--- src/domain/order/service.go
+++ src/domain/order/service.go
@@ -6,5 +6,7 @@ type Service interface {
 	PlaceOrder(o Order) (Order, error)
 	CancelOrder(ID, userID uuid.UUID) (uuid.UUID, error)
 	GetByID(ID uuid.UUID) (Order, error)
-	OrdersByUserID(userID uuid.UUID) ([]Order, error)
+	OrdersByUserID(userID uuid.UUID) ([]Order, error) //Для domain User
+	CalculateTotal(o Order) float64
+	GenerateOrderNum() string
 }
