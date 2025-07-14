e2f4b65c9d696395d3e07fb83f67dc28434c85cb
Andrey <snick25rs@gmail.com>
2025-07-14 05:12:31 +0200

    Удалил импорт uuid и изменил тип ProductID в NewProductResponse со значения uuid.UUID на строку.

--- src/api/product/dto/dto.go
+++ src/api/product/dto/dto.go
@@ -1,21 +1,17 @@
 package productdto
 
-import (
-	"github.com/google/uuid"
-)
-
 type GetProductByIDRequest struct {
 	ID string `uri:"id" binding:"required,uuid"`
 }
 
-type ProductResponse struct {
-	ProductID          uuid.UUID `json:"id"`                                            // Product ID
-	ProductName        string    `json:"name" binding:"required,min=3,max=50"`          // Product name
-	ProductImg         string    `json:"img" binding:"required"`                        // Product image URL
-	ProductPrice       float64   `json:"price" binding:"required,gt=0"`                 // Product price
-	ProductCategory    string    `json:"category" binding:"required,min=3,max=20"`      // Product category
-	ProductDescription string    `json:"description" binding:"required,min=10,max=200"` // Product description
-	ProductStock       int       `json:"stock" binding:"required,gt=0"`                 // Product stock quantity
+type NewProductResponse struct {
+	ProductID          string  `json:"id"`          // Product ID
+	ProductName        string  `json:"name"`        // Product name
+	ProductImg         string  `json:"img"`         // Product image URL
+	ProductPrice       float64 `json:"price"`       // Product price
+	ProductCategory    string  `json:"category"`    // Product category
+	ProductDescription string  `json:"description"` // Product description
+	ProductStock       int     `json:"stock"`       // Product stock quantity
 }
 
 type GetProductsByCategoryRequest struct {
