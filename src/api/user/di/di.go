3862a5b28fcebda940bcf281bb081b942f7e5ef8
Andrey <snick25rs@gmail.com>
2025-07-14 05:11:30 +0200

    Удалил импорт домена пользователя и изменил тип возвращаемого значения GetUserService на userservice.Service.

--- src/api/user/di/di.go
+++ src/api/user/di/di.go
@@ -3,12 +3,11 @@ package userdi
 import (
 	orderdi "shop/src/api/order/di"
 	userservice "shop/src/app/user"
-	"shop/src/domain/user"
 	db "shop/src/infra/db/core"
 	userrepo "shop/src/infra/user"
 )
 
-func GetUserService() user.Service {
+func GetUserService() userservice.Service {
 	database := db.GetDatabase()
 	r := userrepo.NewRepository(database)
 	orderService := orderdi.GetOrderService()
