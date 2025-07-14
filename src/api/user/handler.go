6c166f116d915dd97330a50a16c1a1855523efbb
Andrey <snick25rs@gmail.com>
2025-07-14 05:13:28 +0200

    Добавлен базовый обработчик пользователей с маршрутами.

--- src/api/user/handler.go
+++ src/api/user/handler.go
@@ -1,1 +1,20 @@
 package userhandler
+
+import "github.com/gin-gonic/gin"
+
+func UserHandler(r *gin.Engine) {
+	// Здесь вы можете определить обработчики для маршрутов, связанных с пользователями
+	// Например:
+	r.GET("/users", getUsers)
+	r.POST("/users", createUser)
+
+	r.DELETE("/users/:id", deleteUser)
+
+	// Пример простого обработчика
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "List of users",
+		})
+	})
+
+}
