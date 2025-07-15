9bfa4d95ff68edac3eaf3bfbcb1b2894f394c30c
Andrey <snick25rs@gmail.com>
2025-07-15 03:08:29 +0200

    Добавил маршруты и обработчик для категории в API магазина.

--- start.go
+++ start.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
+	category "shop/src/api/category"
 	product "shop/src/api/product"
 	"shop/src/infra/db"
 )
@@ -17,5 +18,6 @@ func main() {
 	defer db.Close()
 	r := gin.Default()
 	product.ProductHandler(r)
+	category.CategoryHandler(r)
 	r.Run(":8080") // Start the server on port 8080
 }
