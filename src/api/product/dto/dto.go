decc05a00fbd5a6f891ac3241da6a88bb45af5ad
Andrey <snick25rs@gmail.com>
2025-07-15 03:02:29 +0200

    Переименовал структуру `NewProductResponse` в `ProductResponse` и изменил поле `ProductCategory` на `ProductCategoryID` типа UUID.

--- src/api/product/dto/dto.go
+++ src/api/product/dto/dto.go
@@ -4,16 +4,16 @@ type GetProductByIDRequest struct {
 	ID string `uri:"id" binding:"required,uuid"`
 }
 
-type NewProductResponse struct {
+type ProductResponse struct {
 	ProductID          string  `json:"id"`          // Product ID
 	ProductName        string  `json:"name"`        // Product name
 	ProductImg         string  `json:"img"`         // Product image URL
 	ProductPrice       float64 `json:"price"`       // Product price
-	ProductCategory    string  `json:"category"`    // Product category
+	ProductCategoryID  string  `json:"category_id"` // Product category
 	ProductDescription string  `json:"description"` // Product description
 	ProductStock       int     `json:"stock"`       // Product stock quantity
 }
 
 type GetProductsByCategoryRequest struct {
-	Category string `form:"category" binding:"omitempty,min=3,max=20"` // Product category
+	CategoryID string `form:"category" binding:"omitempty,uuid"` // Product category
 }
