fd201fc77c5b49e3ac74b5b2d8eee0d778fdb2ed
Andrey <snick25rs@gmail.com>
2025-07-18 23:40:46 +0200

    Добавил интерфейс хешера паролей в конструктор UserService.

--- src/api/user/di/di.go
+++ src/api/user/di/di.go
@@ -4,6 +4,7 @@ import (
 	"shop/src/api/order/di"
 	"shop/src/app/user"
 	db "shop/src/infra/db/core"
+	hash "shop/src/infra/hasher"
 	r "shop/src/infra/user"
 )
 
@@ -11,5 +12,6 @@ func GetUserService() user.UseCase {
 	database := db.GetDatabase()
 	repo := r.NewRepository(database)
 	orderService := di.GetOrderService()
-	return user.NewUserService(repo, orderService)
+	hasher := hash.NewHasher()
+	return user.NewUserService(repo, orderService, hasher)
 }
