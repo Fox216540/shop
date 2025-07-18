311e71fe3d10cb4aa4effe708696eb2990e7cd59
Andrey <snick25rs@gmail.com>
2025-07-18 23:40:59 +0200

    Добавил валидацию данных в DTO и создал структуры UserResponse и TestDeleteRequest.

--- src/api/user/dto/dto.go
+++ src/api/user/dto/dto.go
@@ -1,9 +1,21 @@
 package dto
 
+import "github.com/google/uuid"
+
 type RegisterRequest struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Name     string `json:"name"`
-	Password string `json:"password"`
-	Address  string `json:"address"`
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Address  string `json:"address" binding:"required"`
+}
+
+type UserResponse struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Message  string    `json:"message"`
+}
+
+type TestDeleteRequest struct {
+	ID string `json:"id" binding:"required,uuid"`
 }
