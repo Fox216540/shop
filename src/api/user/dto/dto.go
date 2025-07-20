07d7712f949174985e94e6e315aa795810d6949b
Andrey <snick25rs@gmail.com>
2025-07-20 22:33:22 +0200

    Добавил структуры DTO для представления ответов и запросов, связанных с продуктами, заказами и элементами заказа.

--- src/api/user/dto/dto.go
+++ src/api/user/dto/dto.go
@@ -16,6 +16,46 @@ type UserResponse struct {
 	Message  string    `json:"message"`
 }
 
+type ProductResponse struct {
+	ProductID          uuid.UUID `json:"id"`
+	ProductName        string    `json:"name"`
+	ProductPrice       float64   `json:"price"`
+	ProductCategoryID  uuid.UUID `json:"category_id"`
+	ProductDescription string    `json:"description"`
+}
+
+type ItemsResponse struct {
+	Product  []ProductResponse `json:"products"`
+	Quantity int               `json:"quantity"`
+}
+
+type OrderResponse struct {
+	ID       uuid.UUID       `json:"id"`
+	UserID   uuid.UUID       `json:"user_id"`
+	OrderNum string          `json:"order_num"`
+	Total    float64         `json:"total"`
+	Status   string          `json:"status"`
+	Items    []ItemsResponse `json:"items"`
+}
+
+type CreateOrderResponse struct {
+	ID       uuid.UUID `json:"id"`
+	UserID   uuid.UUID `json:"user_id"`
+	OrderNum string    `json:"order_num"`
+	Total    float64   `json:"total"`
+	Status   string    `json:"status"`
+}
+
 type TestDeleteRequest struct {
 	ID string `json:"id" binding:"required,uuid"`
+}
+
+type ItemRequest struct {
+	ProductID string `json:"product_id" binding:"required,uuid"`
+	Quantity  int    `json:"quantity" binding:"required"`
+}
+
+type TestCreateOrderRequest struct {
+	ID         string        `json:"id" binding:"required,uuid"`
+	OrderItems []ItemRequest `json:"product_items" binding:"required"`
 }
