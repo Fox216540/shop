76dc83ef7f936d5e1f9de5d50abb58f7aba91bce
Andrey <snick25rs@gmail.com>
2025-07-23 22:35:18 +0200

    Переместил определение `GetProductByIDRequest` и изменил комментарий TODO на "Response".

--- src/api/product/dto/dto.go
+++ src/api/product/dto/dto.go
@@ -1,10 +1,6 @@
 package dto
 
-// TODO: Отсортировать DTO
-
-type GetProductByIDRequest struct {
-	ID string `uri:"id" binding:"required,uuid"`
-}
+// Response
 
 type ProductResponse struct {
 	ProductID          string  `json:"id"`          // Product ID
@@ -14,6 +10,12 @@ type ProductResponse struct {
 	ProductCategoryID  string  `json:"category_id"` // Product category
 	ProductDescription string  `json:"description"` // Product description
 	ProductStock       int     `json:"stock"`       // Product stock quantity
+}
+
+// Request
+
+type GetProductByIDRequest struct {
+	ID string `uri:"id" binding:"required,uuid"`
 }
 
 type GetProductsByCategoryRequest struct {
