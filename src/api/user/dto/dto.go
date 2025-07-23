7131d95eadde89c067fbd2b6afb64f4116f37983
Andrey <snick25rs@gmail.com>
2025-07-23 18:02:52 +0200

    Удалил неиспользуемые DTO, связанные с продуктами и заказами, и добавил структуру запроса для получения заказов с использованием валидации UUID.

--- src/api/user/dto/dto.go
+++ src/api/user/dto/dto.go
@@ -1,6 +1,8 @@
 package dto
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 // TODO: Отсортировать DTO
 
@@ -16,28 +18,6 @@ type UserResponse struct {
 	ID       uuid.UUID `json:"id"`
 	Username string    `json:"username"`
 	Message  string    `json:"message"`
-}
-
-type ProductResponse struct {
-	ProductID          uuid.UUID `json:"id"`
-	ProductName        string    `json:"name"`
-	ProductPrice       float64   `json:"price"`
-	ProductCategoryID  uuid.UUID `json:"category_id"`
-	ProductDescription string    `json:"description"`
-}
-
-type ItemsResponse struct {
-	Product  []ProductResponse `json:"products"`
-	Quantity int               `json:"quantity"`
-}
-
-type OrderResponse struct {
-	ID       uuid.UUID       `json:"id"`
-	UserID   uuid.UUID       `json:"user_id"`
-	OrderNum string          `json:"order_num"`
-	Total    float64         `json:"total"`
-	Status   string          `json:"status"`
-	Items    []ItemsResponse `json:"items"`
 }
 
 type CreateOrderResponse struct {
@@ -57,12 +37,18 @@ type ItemRequest struct {
 	Quantity  int    `json:"quantity" binding:"required"`
 }
 
+// TODO: Поменять product_items на order_items
 type TestCreateOrderRequest struct {
 	ID         string        `json:"id" binding:"required,uuid"`
 	OrderItems []ItemRequest `json:"product_items" binding:"required"`
 }
 
+// TODO: Поменять userID на user_id
 type TestDeleteOrderRequest struct {
 	UserID string `json:"userID" binding:"required,uuid"`
 	ID     string `json:"id" binding:"required,uuid"`
+}
+
+type TestGetOrdersRequest struct {
+	UserID string `json:"userID" binding:"required,uuid"`
 }
