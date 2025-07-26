6a8742c3c59032e827c0bf575ae9bd734d78fd87
Andrey <snick25rs@gmail.com>
2025-07-26 21:59:25 +0300

    Обновил интерфейс UseCase для работы с JWT токенами.

--- src/app/user/usecase.go
+++ src/app/user/usecase.go
@@ -3,14 +3,15 @@ package user
 import (
 	"github.com/google/uuid"
 	"shop/src/api/user/dto"
+	"shop/src/domain/jwt"
 	"shop/src/domain/order"
 	"shop/src/domain/user"
 )
 
 type UseCase interface {
-	Register(userDto dto.RegisterRequest) (user.User, error)
-	Login(usernameOrEmail, password string) (user.User, error)
-	Logout(userID uuid.UUID) error
+	Register(userDto dto.RegisterRequest) (user.User, jwt.Tokens, error)
+	Login(usernameOrEmail, password string) (user.User, jwt.Tokens, error)
+	Logout(userID uuid.UUID, token string) error
 	LogoutAll(userID uuid.UUID) error
 	Update(userID uuid.UUID, u user.User) (user.User, error)
 	Delete(userID uuid.UUID) (user.User, error)
