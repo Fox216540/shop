e633c0c56fd75985bda11e3e690cabf00aed76b8
Andrey <snick25rs@gmail.com>
2025-07-26 21:55:38 +0300

    Добавил JWT-сервис и хранилище токенов для управления сессиями пользователей.

--- src/api/user/di/di.go
+++ src/api/user/di/di.go
@@ -5,13 +5,25 @@ import (
 	"shop/src/app/user"
 	db "shop/src/infra/db/core"
 	hash "shop/src/infra/hasher"
+	"shop/src/infra/jwt"
+	"shop/src/infra/tokenstorage"
 	r "shop/src/infra/user"
 )
 
 func GetUserService() user.UseCase {
 	database := db.GetDatabase()
+	tokenStorage := db.GetRedis()
 	repo := r.NewRepository(database)
 	orderService := di.GetOrderService()
 	hasher := hash.NewHasher()
-	return user.NewUserService(repo, orderService, hasher)
+	jwtService := jwt.NewService()
+	tSRepo := tokenstorage.NewRepository(tokenStorage)
+
+	return user.NewUserService(
+		repo,
+		orderService,
+		hasher,
+		jwtService,
+		tSRepo,
+	)
 }
