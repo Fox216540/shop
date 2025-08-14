ae49dcec27a994808d853f0198086aef46d64a93
Andrey <snick25rs@gmail.com>
2025-08-14 14:41:49 +0300

    Заменил поиск и проверку существования пользователя по имени пользователя или электронной почте на поиск и проверку по номеру телефона или электронной почте и просто по электронной почте соответственно.

--- src/domain/user/repository.go
+++ src/domain/user/repository.go
@@ -6,8 +6,8 @@ type Repository interface {
 	Add(u User) (User, error)
 	Delete(ID uuid.UUID) (uuid.UUID, error)
 	GetByID(ID uuid.UUID) (User, error)
-	FindByUsernameOrEmail(usernameOrEmail string) (User, error)
+	FindByPhoneOrEmail(phoneOrEmail string) (User, error)
 	Update(u User) (User, error)
 	ExistsPhone(phone string) (bool, error)
-	ExistsUsernameOrEmail(usernameOrEmail string) (bool, error)
+	ExistsEmail(email string) (bool, error)
 }
