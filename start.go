15be703e72477e78f661f47cc6dd3fb3316de612
Andrey <snick25rs@gmail.com>
2025-08-14 22:30:18 +0300

    Удалил зависимости godotenv и log, а также связанный с ними код для загрузки файла .env.

--- start.go
+++ start.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"log"
 	"shop/src/api/category"
 	"shop/src/api/product"
 	"shop/src/api/user"
@@ -11,10 +9,6 @@ import (
 )
 
 func main() {
-	err := godotenv.Load() // по умолчанию ищет файл .env в текущей папке
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
 	db.InitPostgres()
 	db.InitRedis()
 	defer db.ClosePostgres()
