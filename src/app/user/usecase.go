a9ee1bc5e544598ff6aa36b82b653a93d5ab446d
Andrey <snick25rs@gmail.com>
2025-08-14 14:42:23 +0300

    Упростил сигнатуры методов `UpdateProfile`, `DeleteOrder` и `CreateOrder` в слое usecase, убрав DTO, где это возможно, и добавил ID пользователя.

--- src/app/user/usecase.go
+++ src/app/user/usecase.go
@@ -13,13 +13,12 @@ type UseCase interface {
 	Login(usernameOrEmail, password string) (user.User, jwt.Tokens, error)
 	Logout(token string) error
 	LogoutAll(token string) error
-	UpdateUsername(userID uuid.UUID, newUsername string) (user.User, jwt.Tokens, error)
 	UpdateEmail(userID uuid.UUID, newEmail string) (user.User, error)
 	UpdatePassword(userID uuid.UUID, newPassword string) (user.User, error)
 	UpdatePhone(userID uuid.UUID, newPhone string) (user.User, error)
-	UpdateProfile(userDTO dto.TestUpdateProfileRequest) (user.User, error)
+	UpdateProfile(userID uuid.UUID, userDTO dto.UpdateProfileRequest) (user.User, error)
 	Delete(userID uuid.UUID) (user.User, error)
 	Orders(userID uuid.UUID) ([]order.Order, error)
-	DeleteOrder(userDTO dto.TestDeleteOrderRequest) (order.Order, error)
-	CreateOrder(userDTO dto.TestCreateOrderRequest) (order.Order, error)
+	DeleteOrder(userID, orderID uuid.UUID) (order.Order, error)
+	CreateOrder(userID uuid.UUID, userDTO dto.CreateOrderRequest) (order.Order, error)
 }
