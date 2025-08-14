35067b750226f4cf72f106493a62fad16208c3a6
Andrey <snick25rs@gmail.com>
2025-08-14 14:40:56 +0300

    Реализовал middleware для аутентификации через JWT, извлекая токен из заголовка Authorization.

--- src/core/middleware/jwt.go
+++ src/core/middleware/jwt.go
@@ -1,1 +1,33 @@
 package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"shop/src/domain/jwt"
+	"strings"
+)
+
+func JWTMiddleware(jwt jwt.Service) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var token string
+		authHeader := c.Request.Header.Get("Authorization")
+		log.Println(authHeader)
+		if strings.HasPrefix(authHeader, "Bearer ") {
+			token = strings.TrimPrefix(authHeader, "Bearer ")
+		}
+		log.Println(token)
+		if token == "" {
+			c.JSON(401, gin.H{"error": "Unauthorized 1"})
+			c.Abort()
+			return
+		}
+		u, err := jwt.DecodeAccessToken(token)
+		if err != nil {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		c.Set("user_id", u.UserID)
+		c.Next()
+	}
+}
