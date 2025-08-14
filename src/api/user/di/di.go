5d1ec10e1749abf5b41226273b0cc99aad226d95
Andrey <snick25rs@gmail.com>
2025-08-14 14:39:48 +0300

    Добавил валидацию формата даты для более строгой проверки входных данных.

--- src/api/user/di/di.go
+++ src/api/user/di/di.go
@@ -1,11 +1,11 @@
 package di
 
 import (
-	"shop/src/api/order/di"
+	jwtdi "shop/src/api/jwt/di"
+	orderdi "shop/src/api/order/di"
 	"shop/src/app/user"
 	db "shop/src/infra/db/core"
 	hash "shop/src/infra/hasher"
-	"shop/src/infra/jwt"
 	"shop/src/infra/tokenstorage"
 	r "shop/src/infra/user"
 )
@@ -14,9 +14,9 @@ func GetUserService() user.UseCase {
 	database := db.GetDatabase()
 	tokenStorage := db.GetRedis()
 	repo := r.NewRepository(database)
-	orderService := di.GetOrderService()
+	orderService := orderdi.GetOrderService()
 	hasher := hash.NewHasher()
-	jwtService := jwt.NewService()
+	jwtService := jwtdi.GetJwtService()
 	tSRepo := tokenstorage.NewRepository(tokenStorage)
 
 	return user.NewUserService(
