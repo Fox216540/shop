4339c9b0752a88da0b77c93258783baa0f72bf32
Andrey <snick25rs@gmail.com>
2025-08-14 14:41:30 +0300

    Удалил столбец Username из модели базы данных и из фиктивных данных.

--- src/infra/user/models/model.go
+++ src/infra/user/models/model.go
@@ -10,7 +10,6 @@ type UserORM struct {
 	gorm.Model
 	ID       int       `gorm:"primaryKey;autoIncrement"`
 	UserID   uuid.UUID `gorm:"type:uuid;not null;uniqueIndex"`
-	Username string    `gorm:"type:varchar(20);not null;uniqueIndex"`
 	Email    string    `gorm:"type:varchar(50);not null;uniqueIndex"`
 	Name     string    `gorm:"type:varchar(20);not null"`
 	Password string    `gorm:"type:varchar(100);not null"`
@@ -25,7 +24,6 @@ func (UserORM) TableName() string {
 func FromORM(orm UserORM) user.User {
 	return user.User{
 		ID:       orm.UserID,
-		Username: orm.Username,
 		Email:    orm.Email,
 		Name:     orm.Name,
 		Password: orm.Password,
