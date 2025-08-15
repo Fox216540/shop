3fd75bd578237edd65652e7e200193e890e996b9
Andrey <snick25rs@gmail.com>
2025-08-15 22:29:32 +0300

    Добавил время жизни (TTL) для токенов при сохранении в хранилище.

--- src/domain/tokenstorage/storage.go
+++ src/domain/tokenstorage/storage.go
@@ -2,10 +2,11 @@ package tokenstorage
 
 import (
 	"github.com/google/uuid"
+	"time"
 )
 
 type TokenStorage interface {
-	Set(jti, userID uuid.UUID) error
+	Set(jti, userID uuid.UUID, ttl time.Duration) error
 	Exists(jti uuid.UUID) (bool, error)
 	Delete(jti, userID uuid.UUID) error
 	DeleteAll(userID uuid.UUID) error
