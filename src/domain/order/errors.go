5f0fdff3e20bb490299a953633a16bad04f23cf8
Andrey <snick25rs@gmail.com>
2025-09-15 02:09:48 +0200

    Реализовал новую структуру `GlobalError` для обработки ошибок с контекстом (домен, слой, сообщение, причина).

--- src/domain/order/errors.go
+++ src/domain/order/errors.go
@@ -1,1 +1,80 @@
 package order
+
+import (
+	"fmt"
+	"shop/src/core/exception"
+)
+
+const (
+	layer  = "Domain"
+	domain = "Order"
+)
+
+type GlobalError struct {
+	*exception.Exception
+	Domain  string
+	Layer   string
+	Message string
+	Err     error
+}
+
+func (e *GlobalError) Error() string {
+	return fmt.Sprintf("Domain: %s\nLayer: %s\nMessage: %s\nCause: %v",
+		e.Domain, e.Layer, e.Message, e.Err)
+}
+
+func (e *GlobalError) Unwrap() error {
+	return e.Err
+}
+
+func NewGlobalError(msg string, err error, layer string) *GlobalError {
+	return &GlobalError{
+		Exception: &exception.Exception{},
+		Domain:    domain,
+		Layer:     layer,
+		Message:   msg,
+		Err:       err,
+	}
+}
+
+type DomainError struct {
+	*GlobalError
+}
+
+func (e *DomainError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewDomainError(msg string, err error) *DomainError {
+	return &DomainError{
+		GlobalError: NewGlobalError(msg, err, layer),
+	}
+}
+
+type NotFoundError struct {
+	*DomainError
+}
+
+func (e *NotFoundError) Error() string {
+	return e.DomainError.Error()
+}
+
+func NewNotFoundError(msg string, err error) *NotFoundError {
+	return &NotFoundError{
+		DomainError: NewDomainError(msg, err),
+	}
+}
+
+type NotFoundOrderError struct {
+	*NotFoundError
+}
+
+func (e *NotFoundOrderError) Error() string {
+	return e.NotFoundError.Error()
+}
+
+func NewNotFoundOrderError(err error) error {
+	return &NotFoundOrderError{
+		NotFoundError: NewNotFoundError("Order not found", err),
+	}
+}
--- src/infra/order/errors.go
+++ src/infra/order/errors.go
@@ -1,1 +1,91 @@
 package order
+
+import (
+	"shop/src/domain/order"
+)
+
+const layer = "Infra"
+
+type ServerError struct {
+	*order.GlobalError
+}
+
+func (e *ServerError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewServerError(msg string, err error) *ServerError {
+	return &ServerError{
+		GlobalError: order.NewGlobalError(msg, err, layer),
+	}
+}
+
+type InvalidSaveOrder struct {
+	*ServerError
+}
+
+func (e *InvalidSaveOrder) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidSaveOrder(err error) error {
+	return &InvalidSaveOrder{
+		ServerError: NewServerError("Invalid Save Order Error", err),
+	}
+}
+
+type InvalidRemoveOrder struct {
+	*ServerError
+}
+
+func (e *InvalidRemoveOrder) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidRemoveOrder(err error) error {
+	return &InvalidRemoveOrder{
+		ServerError: NewServerError("Invalid Remove Order Error", err),
+	}
+}
+
+type InvalidGetOrderByID struct {
+	*ServerError
+}
+
+func (e *InvalidGetOrderByID) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidGetOrderByID(err error) error {
+	return &InvalidGetOrderByID{
+		ServerError: NewServerError("Invalid Get Order By ID Error", err),
+	}
+}
+
+type InvalidGetOrdersByUserID struct {
+	*ServerError
+}
+
+func (e *InvalidGetOrdersByUserID) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidGetOrdersByUserID(err error) error {
+	return &InvalidGetOrdersByUserID{
+		ServerError: NewServerError("Invalid Get Orders By User ID Error", err),
+	}
+}
+
+type InvalidCheckOrderNum struct {
+	*ServerError
+}
+
+func (e *InvalidCheckOrderNum) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidCheckOrderNum(err error) error {
+	return &InvalidCheckOrderNum{
+		ServerError: NewServerError("Invalid Check Order Number Error", err),
+	}
+}
