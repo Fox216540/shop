c3afee6b6b781c28aa0d73daa887e8b4b54ff605
Andrey <snick25rs@gmail.com>
2025-09-15 02:11:25 +0200

    Реализовал пользовательские типы ошибок в слоях Domain и Infra, чтобы обеспечить более структурированную обработку ошибок.

--- src/domain/user/errors.go
+++ src/domain/user/errors.go
@@ -1,1 +1,80 @@
 package user
+
+import (
+	"fmt"
+	"shop/src/core/exception"
+)
+
+const (
+	layer  = "Domain"
+	domain = "User"
+)
+
+type GlobalError struct {
+	*exception.Exception
+	Domain  string
+	Layer   string
+	Message string
+	Err     error
+}
+
+func (e *GlobalError) Error() string {
+	return fmt.Sprintf("Domain: %s\nLayer: %s\nMessage: %s\nCause: %v",
+		e.Domain, e.Layer, e.Message, e.Err)
+}
+
+func (e *GlobalError) Unwrap() error {
+	return e.Err
+}
+
+func NewGlobalError(msg string, err error, layer string) *GlobalError {
+	return &GlobalError{
+		Exception: &exception.Exception{},
+		Domain:    domain,
+		Layer:     layer,
+		Message:   msg,
+		Err:       err,
+	}
+}
+
+type DomainError struct {
+	*GlobalError
+}
+
+func (e *DomainError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewDomainError(msg string, err error) *DomainError {
+	return &DomainError{
+		GlobalError: NewGlobalError(msg, err, layer),
+	}
+}
+
+type NotFoundError struct {
+	*DomainError
+}
+
+func (e *NotFoundError) Error() string {
+	return e.DomainError.Error()
+}
+
+func NewNotFoundError(msg string, err error) *NotFoundError {
+	return &NotFoundError{
+		DomainError: NewDomainError(msg, err),
+	}
+}
+
+type NotFoundUserError struct {
+	*NotFoundError
+}
+
+func (e *NotFoundUserError) Error() string {
+	return e.NotFoundError.Error()
+}
+
+func NewNotFoundUserError(err error) error {
+	return &NotFoundUserError{
+		NotFoundError: NewNotFoundError("User not found", err),
+	}
+}
--- src/infra/user/errors.go
+++ src/infra/user/errors.go
@@ -1,1 +1,119 @@
 package user
+
+import (
+	"shop/src/domain/user"
+)
+
+const layer = "Infra"
+
+type ServerError struct {
+	*user.GlobalError
+}
+
+func (e *ServerError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewServerError(msg string, err error) *ServerError {
+	return &ServerError{
+		GlobalError: user.NewGlobalError(msg, err, layer),
+	}
+}
+
+type InvalidAdd struct {
+	*ServerError
+}
+
+func (e *InvalidAdd) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidAdd(err error) error {
+	return &InvalidAdd{
+		ServerError: NewServerError("Invalid Add Error", err),
+	}
+}
+
+type InvalidDelete struct {
+	*ServerError
+}
+
+func (e *InvalidDelete) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidDelete(err error) error {
+	return &InvalidDelete{
+		ServerError: NewServerError("Invalid Delete Error", err),
+	}
+}
+
+type InvalidGetByID struct {
+	*ServerError
+}
+
+func (e *InvalidGetByID) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidGetByID(err error) error {
+	return &InvalidGetByID{
+		ServerError: NewServerError("Invalid Get By ID Error", err),
+	}
+}
+
+type InvalidFindByPhoneOrEmail struct {
+	*ServerError
+}
+
+func (e *InvalidFindByPhoneOrEmail) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidFindByPhoneOrEmail(err error) error {
+	return &InvalidFindByPhoneOrEmail{
+		ServerError: NewServerError("Invalid Find By Phone Or Email Error", err),
+	}
+}
+
+type InvalidUpdate struct {
+	*ServerError
+}
+
+func (e *InvalidUpdate) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidUpdate(err error) error {
+	return &InvalidUpdate{
+		ServerError: NewServerError("Invalid Update Error", err),
+	}
+}
+
+type InvalidExistsPhone struct {
+	*ServerError
+}
+
+func (e *InvalidExistsPhone) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidExistsPhone(err error) error {
+	return &InvalidExistsPhone{
+		ServerError: NewServerError("Invalid Exists Phone Error", err),
+	}
+}
+
+type InvalidExistsEmail struct {
+	*ServerError
+}
+
+func (e *InvalidExistsEmail) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidExistsEmail(err error) error {
+	return &InvalidExistsEmail{
+		ServerError: NewServerError("Invalid Exists Email Error", err),
+	}
+}
