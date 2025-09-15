251fd4df3b274fce0d1a0ddd7ac3c980ece44b5a
Andrey <snick25rs@gmail.com>
2025-09-15 02:10:30 +0200

    Реализовал базовую структуру ошибок для домена Category, включая общие, доменные и специфичные ошибки отсутствия категории.

--- src/domain/category/errors.go
+++ src/domain/category/errors.go
@@ -1,1 +1,80 @@
 package category
+
+import (
+	"fmt"
+	"shop/src/core/exception"
+)
+
+const (
+	layer  = "Domain"
+	domain = "Category"
+)
+
+type GlobalError struct {
+	*exception.Exception
+	Domain  string
+	Layer   string
+	Message string
+	Err     error
+}
+
+func (e *GlobalError) Error() string {
+	return fmt.Sprintf("Domain: %s\nLayer: %s\nMessage: %s\nCause: %v",
+		e.Domain, e.Layer, e.Message, e.Err)
+}
+
+func (e *GlobalError) Unwrap() error {
+	return e.Err
+}
+
+func NewGlobalError(msg string, err error, layer string) *GlobalError {
+	return &GlobalError{
+		Exception: &exception.Exception{},
+		Domain:    domain,
+		Layer:     layer,
+		Message:   msg,
+		Err:       err,
+	}
+}
+
+type DomainError struct {
+	*GlobalError
+}
+
+func (e *DomainError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewDomainError(msg string, err error) *DomainError {
+	return &DomainError{
+		GlobalError: NewGlobalError(msg, err, layer),
+	}
+}
+
+type NotFoundError struct {
+	*DomainError
+}
+
+func (e *NotFoundError) Error() string {
+	return e.DomainError.Error()
+}
+
+func NewNotFoundError(msg string, err error) *NotFoundError {
+	return &NotFoundError{
+		DomainError: NewDomainError(msg, err),
+	}
+}
+
+type NotFoundCategoryError struct {
+	*NotFoundError
+}
+
+func (e *NotFoundCategoryError) Error() string {
+	return e.NotFoundError.Error()
+}
+
+func NewNotFoundCategoryError(err error) error {
+	return &NotFoundCategoryError{
+		NotFoundError: NewNotFoundError("Category not found", err),
+	}
+}
--- src/infra/category/errors.go
+++ src/infra/category/errors.go
@@ -1,1 +1,35 @@
 package category
+
+import (
+	"shop/src/domain/category"
+)
+
+const layer = "Infra"
+
+type ServerError struct {
+	*category.GlobalError
+}
+
+func (e *ServerError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewServerError(msg string, err error) *ServerError {
+	return &ServerError{
+		GlobalError: category.NewGlobalError(msg, err, layer),
+	}
+}
+
+type InvalidFindAllError struct {
+	*ServerError
+}
+
+func (e *InvalidFindAllError) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidFindAllError(err error) error {
+	return &InvalidFindAllError{
+		ServerError: NewServerError("Invalid Find All Error", err),
+	}
+}
