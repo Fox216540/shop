c6a67257ab86809d59bdc7c55eb158fc4183087d
Andrey <snick25rs@gmail.com>
2025-09-15 02:12:06 +0200

    Добавил обработку ошибки `gorm.ErrRecordNotFound` при поиске всех категорий и вернул более конкретную ошибку.

--- src/infra/category/repository.go
+++ src/infra/category/repository.go
@@ -1,6 +1,7 @@
 package category
 
 import (
+	"errors"
 	"gorm.io/gorm"
 	"shop/src/domain/category"
 	"shop/src/infra/category/models"
@@ -18,7 +19,10 @@ func NewRepository(db *db.Database) category.Repository {
 func (r *repository) FindAll() ([]category.Category, error) {
 	var categoriesORM []models.CategoryORM
 	if err := r.db.Find(&categoriesORM).Error; err != nil {
-		return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, category.NewNotFoundCategoryError(err)
+		}
+		return nil, NewInvalidFindAllError(err)
 	}
 
 	categories := make([]category.Category, 0, len(categoriesORM))
