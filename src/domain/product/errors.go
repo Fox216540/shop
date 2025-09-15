f6466cd7bd505c641f67a233b14d9e2f61ef9ec9
Andrey <snick25rs@gmail.com>
2025-09-15 02:10:16 +0200

    Реализовал обработку ошибок и добавил кастомные типы ошибок для доменной логики и инфраструктурного слоя.

--- src/domain/product/errors.go
+++ src/domain/product/errors.go
@@ -1,1 +1,94 @@
 package product
+
+import (
+	"fmt"
+	"shop/src/core/exception"
+)
+
+const (
+	layer  = "Domain"
+	domain = "Product"
+)
+
+type GlobalError struct {
+	*exception.Exception
+	Domain  string
+	Layer   string
+	Message string
+	Err     error
+}
+
+func (e *GlobalError) Error() string {
+	return fmt.Sprintf("Domain: %s\nLayer: %s\nMessage: %s\nCause: %v",
+		e.Domain, e.Layer, e.Message, e.Err)
+}
+
+func (e *GlobalError) Unwrap() error {
+	return e.Err
+}
+
+func NewGlobalError(msg string, err error, layer string) *GlobalError {
+	return &GlobalError{
+		Exception: &exception.Exception{},
+		Domain:    domain,
+		Layer:     layer,
+		Message:   msg,
+		Err:       err,
+	}
+}
+
+type DomainError struct {
+	*GlobalError
+}
+
+func (e *DomainError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewDomainError(msg string, err error) *DomainError {
+	return &DomainError{
+		GlobalError: NewGlobalError(msg, err, layer),
+	}
+}
+
+type NotFoundError struct {
+	*DomainError
+}
+
+func (e *NotFoundError) Error() string {
+	return e.DomainError.Error()
+}
+
+func NewNotFoundError(msg string, err error) *NotFoundError {
+	return &NotFoundError{
+		DomainError: NewDomainError(msg, err),
+	}
+}
+
+type NotFoundProductError struct {
+	*NotFoundError
+}
+
+func (e *NotFoundProductError) Error() string {
+	return e.NotFoundError.Error()
+}
+
+func NewNotFoundProductError(err error) error {
+	return &NotFoundProductError{
+		NotFoundError: NewNotFoundError("Product not found", err),
+	}
+}
+
+type NotFoundProductsError struct {
+	*NotFoundError
+}
+
+func (e *NotFoundProductsError) Error() string {
+	return e.NotFoundError.Error()
+}
+
+func NewNotFoundProductsError(err error) error {
+	return &NotFoundProductsError{
+		NotFoundError: NewNotFoundError("Products not found", err),
+	}
+}
--- src/infra/product/errors.go
+++ src/infra/product/errors.go
@@ -1,1 +1,63 @@
 package product
+
+import (
+	"shop/src/domain/product"
+)
+
+const layer = "Infra"
+
+type ServerError struct {
+	*product.GlobalError
+}
+
+func (e *ServerError) Error() string {
+	return e.GlobalError.Error()
+}
+
+func NewServerError(msg string, err error) *ServerError {
+	return &ServerError{
+		GlobalError: product.NewGlobalError(msg, err, layer),
+	}
+}
+
+type InvalidFindProductsByCategoryID struct {
+	*ServerError
+}
+
+func (e *InvalidFindProductsByCategoryID) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidFindProductsByCategoryID(err error) error {
+	return &InvalidFindProductsByCategoryID{
+		ServerError: NewServerError("Invalid Find Products By Category ID Error", err),
+	}
+}
+
+type InvalidFindProductByID struct {
+	*ServerError
+}
+
+func (e *InvalidFindProductByID) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidFindProductByID(err error) error {
+	return &InvalidFindProductByID{
+		ServerError: NewServerError("Invalid Find Product By ID Error", err),
+	}
+}
+
+type InvalidFindProductsByIDs struct {
+	*ServerError
+}
+
+func (e *InvalidFindProductsByIDs) Error() string {
+	return e.ServerError.Error()
+}
+
+func NewInvalidFindProductsByIDs(err error) error {
+	return &InvalidFindProductsByIDs{
+		ServerError: NewServerError("Invalid Find Products By IDs Error", err),
+	}
+}
