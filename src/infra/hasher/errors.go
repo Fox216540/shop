b45567bf9a03a3748034bc2e39f6650b6f8e85c2
Andrey <snick25rs@gmail.com>
2025-09-30 23:19:36 +0200

    Удалил структуру `InvalidVerifyError` и связанные с ней функции, поскольку она оказалась избыточной.

--- src/infra/hasher/errors.go
+++ src/infra/hasher/errors.go
@@ -33,17 +33,3 @@ func NewInvalidHashError(err error) error {
 		HasherServerError: NewHasherServerError("Invalid Find All Error", err),
 	}
 }
-
-type InvalidVerifyError struct {
-	*HasherServerError
-}
-
-func (e *InvalidVerifyError) Error() string {
-	return e.HasherServerError.Error()
-}
-
-func NewInvalidVerifyError(err error) error {
-	return &InvalidVerifyError{
-		HasherServerError: NewHasherServerError("Invalid Verify Error", err),
-	}
-}
