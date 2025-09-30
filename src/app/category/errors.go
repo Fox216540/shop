c3e7fe6ec35bc7d8544c4ba5bf5fe02b6eb31131
Andrey <snick25rs@gmail.com>
2025-09-30 23:16:26 +0200

    Добавил собственные типы ошибок для модуля категорий, унаследованные от базового типа ошибок приложения.

--- src/app/category/errors.go
+++ src/app/category/errors.go
@@ -1,1 +1,33 @@
 package category
+
+import "shop/src/app/globalError"
+
+const domain = "Category"
+
+type CategoryServerError struct {
+	*globalError.AppServerError
+}
+
+func (e *CategoryServerError) Error() string {
+	return e.AppServerError.Error()
+}
+
+func NewCategoryServerError(msg string, err error) *CategoryServerError {
+	return &CategoryServerError{
+		AppServerError: globalError.NewAppServerError(msg, domain, err),
+	}
+}
+
+type InvalidGetCategories struct {
+	*CategoryServerError
+}
+
+func (e *InvalidGetCategories) Error() string {
+	return e.CategoryServerError.Error()
+}
+
+func NewInvalidGetCategories(err error) error {
+	return &InvalidGetCategories{
+		CategoryServerError: NewCategoryServerError("Invalid Get Categories", err),
+	}
+}
