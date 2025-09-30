2e63c4d60beaaa5629f600366f3f420d51b5a499
Andrey <snick25rs@gmail.com>
2025-09-30 23:16:56 +0200

    Добавил типы ошибок для сервиса категорий продуктов, включая `CategoryServerError`, `InvalidProductsOfCategoryID`, `InvalidProductByID` и `InvalidProductsByIDs`.

--- src/app/product/errors.go
+++ src/app/product/errors.go
@@ -1,1 +1,61 @@
 package product
+
+import "shop/src/app/globalError"
+
+const domain = "Product"
+
+type CategoryServerError struct {
+	*globalError.AppServerError
+}
+
+func (e *CategoryServerError) Error() string {
+	return e.AppServerError.Error()
+}
+
+func NewCategoryServerError(msg string, err error) *CategoryServerError {
+	return &CategoryServerError{
+		AppServerError: globalError.NewAppServerError(msg, domain, err),
+	}
+}
+
+type InvalidProductsOfCategoryID struct {
+	*CategoryServerError
+}
+
+func (e *InvalidProductsOfCategoryID) Error() string {
+	return e.CategoryServerError.Error()
+}
+
+func NewInvalidProductsOfCategoryID(err error) error {
+	return &InvalidProductsOfCategoryID{
+		CategoryServerError: NewCategoryServerError("Invalid Products Of Category ID", err),
+	}
+}
+
+type InvalidProductByID struct {
+	*CategoryServerError
+}
+
+func (e *InvalidProductByID) Error() string {
+	return e.CategoryServerError.Error()
+}
+
+func NewInvalidProductByID(err error) error {
+	return &InvalidProductByID{
+		CategoryServerError: NewCategoryServerError("Invalid Product By ID", err),
+	}
+}
+
+type InvalidProductsByIDs struct {
+	*CategoryServerError
+}
+
+func (e *InvalidProductsByIDs) Error() string {
+	return e.CategoryServerError.Error()
+}
+
+func NewInvalidProductsByIDs(err error) error {
+	return &InvalidProductsByIDs{
+		CategoryServerError: NewCategoryServerError("Invalid Products By IDs", err),
+	}
+}
