2c58ee384025e950ddd2a2122fe20cecfa14c828
Andrey <snick25rs@gmail.com>
2025-09-30 23:18:49 +0200

    Переименовал `CategoryServerError` в `ProductServerError` в файле `errors.go`, чтобы отразить его фактическое использование.

--- src/app/product/errors.go
+++ src/app/product/errors.go
@@ -4,58 +4,58 @@ import "shop/src/app/globalError"
 
 const domain = "Product"
 
-type CategoryServerError struct {
+type ProductServerError struct {
 	*globalError.AppServerError
 }
 
-func (e *CategoryServerError) Error() string {
+func (e *ProductServerError) Error() string {
 	return e.AppServerError.Error()
 }
 
-func NewCategoryServerError(msg string, err error) *CategoryServerError {
-	return &CategoryServerError{
+func NewCategoryServerError(msg string, err error) *ProductServerError {
+	return &ProductServerError{
 		AppServerError: globalError.NewAppServerError(msg, domain, err),
 	}
 }
 
 type InvalidProductsOfCategoryID struct {
-	*CategoryServerError
+	*ProductServerError
 }
 
 func (e *InvalidProductsOfCategoryID) Error() string {
-	return e.CategoryServerError.Error()
+	return e.ProductServerError.Error()
 }
 
 func NewInvalidProductsOfCategoryID(err error) error {
 	return &InvalidProductsOfCategoryID{
-		CategoryServerError: NewCategoryServerError("Invalid Products Of Category ID", err),
+		ProductServerError: NewCategoryServerError("Invalid Products Of Category ID", err),
 	}
 }
 
 type InvalidProductByID struct {
-	*CategoryServerError
+	*ProductServerError
 }
 
 func (e *InvalidProductByID) Error() string {
-	return e.CategoryServerError.Error()
+	return e.ProductServerError.Error()
 }
 
 func NewInvalidProductByID(err error) error {
 	return &InvalidProductByID{
-		CategoryServerError: NewCategoryServerError("Invalid Product By ID", err),
+		ProductServerError: NewCategoryServerError("Invalid Product By ID", err),
 	}
 }
 
 type InvalidProductsByIDs struct {
-	*CategoryServerError
+	*ProductServerError
 }
 
 func (e *InvalidProductsByIDs) Error() string {
-	return e.CategoryServerError.Error()
+	return e.ProductServerError.Error()
 }
 
 func NewInvalidProductsByIDs(err error) error {
 	return &InvalidProductsByIDs{
-		CategoryServerError: NewCategoryServerError("Invalid Products By IDs", err),
+		ProductServerError: NewCategoryServerError("Invalid Products By IDs", err),
 	}
 }
