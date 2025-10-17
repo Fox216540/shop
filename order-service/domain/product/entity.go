c68e2ed968ea105eae772c66ce951952f967b465
Andrey <snick25rs@gmail.com>
2025-10-18 00:49:05 +0200

    Добавил структуру `Entity` для представления базовых объектов в системе.

---
+++ order-service/domain/product/entity.go
@@ -0,0 +1,10 @@
+package product
+
+import "github.com/google/uuid"
+
+type Product struct {
+	ID    uuid.UUID // Product ID
+	Name  string    // Product name
+	Img   string    // Product image URL
+	Price float64   // Product price
+}
