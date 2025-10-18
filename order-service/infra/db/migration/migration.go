d39fc4328420506997cbc11d020ba35c7bd623c8
Andrey <snick25rs@gmail.com>
2025-10-19 01:57:57 +0200

    Добавил новый файл `migration.go` для управления миграциями базы данных.

---
+++ order-service/infra/db/migration/migration.go
@@ -0,0 +1,16 @@
+package migration
+
+import (
+	"github.com/Fox216540/shop/order-service/infra/order/models"
+	"gorm.io/gorm"
+)
+
+func Migration(db *gorm.DB) error {
+	//if err := db.Exec(`CREATE EXTENSION IF NOT EXISTS "pgcrypto"`).Error; err != nil {
+	//	return fmt.Errorf("failed to create extension pgcrypto: %w", err)
+	//}
+
+	return db.AutoMigrate(
+		&models.OrderORM{},
+		&models.OrderProductORM{})
+}
