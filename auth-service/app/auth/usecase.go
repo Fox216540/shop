2200bcc7a24d3500b75eb25fe7bba1a601f77bdc
Andrey <snick25rs@gmail.com>
2025-10-21 03:19:54 +0200

    Заменил методы SignUp, Login, NewPassword на GenerateTokens для упрощения процесса аутентификации и удаления прямой работы с паролями в usecase.

--- auth-service/app/auth/usecase.go
+++ auth-service/app/auth/usecase.go
@@ -1,17 +1,15 @@
 package auth
 
 import (
-	"github.com/Fox216540/shop/auth-service/domain/auth"
 	"github.com/Fox216540/shop/auth-service/domain/jwt"
+	"github.com/google/uuid"
 )
 
 type UseCase interface {
-	SignUp(auth auth.Auth) (hash string, tokens jwt.Tokens, err error)
-	Login(auth auth.Auth, hash string) (tokens jwt.Tokens, err error)
-	DecodeRefreshToken(token string) (userJWT jwt.JWTUser, err error)
+	GenerateTokens(userID uuid.UUID) (tokens jwt.Tokens, err error)
 	DecodeAccessToken(token string) (userJWT jwt.JWTUser, err error)
+	DecodeRefreshToken(token string) (userJWT jwt.JWTUser, err error)
 	DeleteRefreshToken(token string) error
 	DeleteAllTokens(token string) error
-	NewPassword(newPassword string) (hash string, err error)
 	RefreshTokens(token string) (tokens jwt.Tokens, err error)
 }
