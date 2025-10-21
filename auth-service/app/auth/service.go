d7e9ccd9a8ab83d6bd472168439d366df055abad
Andrey <snick25rs@gmail.com>
2025-10-21 03:19:39 +0200

    Удалил методы SignUp, Login и NewPassword, убрал hasher из зависимостей сервиса и переименовал метод generateTokens в GenerateTokens.

--- auth-service/app/auth/service.go
+++ auth-service/app/auth/service.go
@@ -1,30 +1,25 @@
 package auth
 
 import (
-	"github.com/Fox216540/shop/auth-service/app/hasher"
 	"github.com/Fox216540/shop/auth-service/app/jwt"
 	"github.com/Fox216540/shop/auth-service/app/tokenstorage"
-	"github.com/Fox216540/shop/auth-service/domain/auth"
 	jwtDomain "github.com/Fox216540/shop/auth-service/domain/jwt"
 	"github.com/google/uuid"
 )
 
 type service struct {
 	jwtUseCase   jwt.UseCase
-	hasher       hasher.UseCase
 	tokenStorage tokenstorage.UseCase
 }
 
-func NewService(js jwt.UseCase, hs hasher.UseCase, ts tokenstorage.UseCase) UseCase {
+func NewService(js jwt.UseCase, ts tokenstorage.UseCase) UseCase {
 	return &service{
 		jwtUseCase:   js,
-		hasher:       hs,
 		tokenStorage: ts,
 	}
 }
 
-func (s *service) generateTokens(userID uuid.UUID) (tokens jwtDomain.Tokens, err error) {
-
+func (s *service) GenerateTokens(userID uuid.UUID) (tokens jwtDomain.Tokens, err error) {
 	refreshToken, jti, err := s.jwtUseCase.GenerateRefresh(userID)
 	if err != nil {
 		return tokens, err
@@ -42,41 +37,16 @@ func (s *service) generateTokens(userID uuid.UUID) (tokens jwtDomain.Tokens, err
 	}, nil
 }
 
-func (s *service) SignUp(a auth.Auth) (hash string, tokens jwtDomain.Tokens, err error) {
-	hash, err = s.NewPassword(a.Password)
-	if err != nil {
-		return "", tokens, err
-	}
-
-	tokens, err = s.generateTokens(a.UserID)
-	if err != nil {
-		return "", tokens, err
-	}
-	return hash, tokens, nil
-}
-
-func (s *service) Login(a auth.Auth, hash string) (tokens jwtDomain.Tokens, err error) {
-	if err := s.hasher.VerifyPass(a.Password, hash); err != nil {
-		return tokens, err
-	}
-
-	tokens, err = s.generateTokens(a.UserID)
-	if err != nil {
-		return tokens, err
-	}
-	return tokens, nil
-}
-
-func (s *service) DecodeRefreshToken(token string) (jwtUser jwtDomain.JWTUser, err error) {
-	jwtUser, err = s.jwtUseCase.DecodeRefresh(token)
+func (s *service) DecodeAccessToken(token string) (jwtUser jwtDomain.JWTUser, err error) {
+	jwtUser, err = s.jwtUseCase.DecodeAccess(token)
 	if err != nil {
 		return jwtUser, err
 	}
 	return jwtUser, nil
 }
 
-func (s *service) DecodeAccessToken(token string) (jwtUser jwtDomain.JWTUser, err error) {
-	jwtUser, err = s.jwtUseCase.DecodeAccess(token)
+func (s *service) DecodeRefreshToken(token string) (jwtUser jwtDomain.JWTUser, err error) {
+	jwtUser, err = s.jwtUseCase.DecodeRefresh(token)
 	if err != nil {
 		return jwtUser, err
 	}
@@ -99,18 +69,13 @@ func (s *service) DeleteAllTokens(token string) error {
 	return s.tokenStorage.DeleteAll(jwtUser.UserID)
 }
 
-func (s *service) NewPassword(newPassword string) (hash string, err error) {
-	return s.hasher.HashPass(newPassword)
-}
-
 func (s *service) RefreshTokens(token string) (tokens jwtDomain.Tokens, err error) {
-	jwtUser, err := s.jwtUseCase.DecodeRefresh(token)
+	userJWT, err := s.DecodeRefreshToken(token)
 	if err != nil {
-		return jwtDomain.Tokens{}, err
+		return tokens, err
 	}
-	if err := s.tokenStorage.Delete(jwtUser.JTI, jwtUser.UserID); err != nil {
-		return jwtDomain.Tokens{}, err
+	if err := s.tokenStorage.Delete(userJWT.JTI, userJWT.UserID); err != nil {
+		return tokens, err
 	}
-	tokens, err = s.generateTokens(jwtUser.UserID)
-	return tokens, nil
+	return s.GenerateTokens(userJWT.UserID)
 }
