885712d7061a7a679cae0247d2b3adc134de8697
Andrey <snick25rs@gmail.com>
2025-10-22 02:20:57 +0200

    Выполнил рефакторинг gRPC-обработчика заказов, заменив использование идентификатора пользователя в запросах на получение из метаданных контекста и обновив маппинг данных для соответствия новым proto-типам.

--- order-service/api/gRPCHandler.go
+++ order-service/api/gRPCHandler.go
@@ -2,11 +2,15 @@ package api
 
 import (
 	"context"
+	"errors"
 	order "github.com/Fox216540/shop/order-service/app"
 	"github.com/Fox216540/shop/order-service/app/dto"
 	orderDomain "github.com/Fox216540/shop/order-service/domain/order"
+	types "github.com/Fox216540/shop/proto/common/gen"
 	pb "github.com/Fox216540/shop/proto/order-service/gen"
 	"github.com/google/uuid"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type GRPCHandler struct {
@@ -14,11 +18,11 @@ type GRPCHandler struct {
 	pb.UnimplementedInternalOrderServiceServer
 }
 
-func (h *GRPCHandler) mapOrder(order orderDomain.Order) *pb.Order {
-	items := make([]*pb.Item, 0, len(order.OrderItems))
+func (h *GRPCHandler) mapOrder(order orderDomain.Order) *types.Order {
+	items := make([]*types.Item, 0, len(order.OrderItems))
 	for _, item := range order.OrderItems {
-		items = append(items, &pb.Item{
-			Product: &pb.Product{
+		items = append(items, &types.Item{
+			Product: &types.Product{
 				Id:    item.Product.ID.String(),
 				Name:  item.Product.Name,
 				Img:   item.Product.Img,
@@ -27,18 +31,42 @@ func (h *GRPCHandler) mapOrder(order orderDomain.Order) *pb.Order {
 			Quantity: item.Quantity,
 		})
 	}
-	return &pb.Order{
+	return &types.Order{
 		Id:       order.ID.String(),
 		OrderNum: order.OrderNum,
-		UserId:   order.UserID.String(),
 		Items:    items,
 		Total:    order.Total,
 		Status:   order.Status,
 	}
 }
 
-func (h *GRPCHandler) PlaceOrder(ctx context.Context, req *pb.PlaceOrderRequest) (*pb.Order, error) {
-	userID, err := uuid.Parse(req.UserId)
+func (h *GRPCHandler) mapOrderForListToResponse(orders []orderDomain.Order) []*pb.OrderForList {
+	ordersForList := make([]*pb.OrderForList, 0, len(orders))
+	for _, o := range orders {
+		ordersForList = append(ordersForList, &pb.OrderForList{
+			Id:       o.ID.String(),
+			OrderNum: o.OrderNum,
+			Total:    o.Total,
+			Status:   o.Status,
+		})
+	}
+	return ordersForList
+}
+
+func (h *GRPCHandler) getUserIDFromMetadata(ctx context.Context) (uuid.UUID, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return uuid.Nil, errors.New("metadata not found")
+	}
+	userID, err := uuid.Parse(md.Get("user_id")[0])
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return userID, nil
+}
+
+func (h *GRPCHandler) CreteOrder(ctx context.Context, req *pb.CreateOrderRequest) (*types.Order, error) {
+	userID, err := h.getUserIDFromMetadata(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -58,54 +86,26 @@ func (h *GRPCHandler) PlaceOrder(ctx context.Context, req *pb.PlaceOrderRequest)
 	return h.mapOrder(o), nil
 }
 
-func (h *GRPCHandler) CancelOrder(ctx context.Context, req *pb.CancelOrderRequest) (*pb.CancelOrderResponse, error) {
-	orderID, err := uuid.Parse(req.OrderId)
+func (h *GRPCHandler) GetOrderById(ctx context.Context, req *types.OrderId) (*types.Order, error) {
+	userID, err := h.getUserIDFromMetadata(ctx)
 	if err != nil {
 		return nil, err
 	}
-	userID, err := uuid.Parse(req.UserId)
+	orderID, err := uuid.Parse(req.Id)
 	if err != nil {
 		return nil, err
 	}
-	orderID, err = h.orderUC.Cancel(orderID, userID)
+	o, err := h.orderUC.GetByIDAndUserID(orderID, userID)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &pb.CancelOrderResponse{
-		OrderId: orderID.String(),
-	}, nil
-}
-
-func (h *GRPCHandler) GetOrderById(ctx context.Context, req *pb.GetOrderByIdRequest) (*pb.Order, error) {
-	orderID, err := uuid.Parse(req.OrderId)
-	if err != nil {
-		return nil, err
-	}
-	o, err := h.orderUC.GetByID(orderID)
-	if err != nil {
-		return nil, err
-	}
 	return h.mapOrder(o), nil
 }
 
-func (h *GRPCHandler) mapOrderForListToResponse(orders []orderDomain.Order) []*pb.OrderForList {
-	ordersForList := make([]*pb.OrderForList, 0, len(orders))
-	for _, o := range orders {
-		ordersForList = append(ordersForList, &pb.OrderForList{
-			Id:       o.ID.String(),
-			OrderNum: o.OrderNum,
-			UserId:   o.UserID.String(),
-			Total:    o.Total,
-			Status:   o.Status,
-		})
-	}
-	return ordersForList
-}
-
-func (h *GRPCHandler) GetOrdersByUserId(ctx context.Context, req *pb.GetOrdersByUserIdRequest) (*pb.GetOrdersByUserIdResponse, error) {
-	userID, err := uuid.Parse(req.UserId)
+func (h *GRPCHandler) GetOrdersByUserId(ctx context.Context, req *emptypb.Empty) (*pb.GetOrdersByUserIdResponse, error) {
+	userID, err := h.getUserIDFromMetadata(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -115,6 +115,24 @@ func (h *GRPCHandler) GetOrdersByUserId(ctx context.Context, req *pb.GetOrdersBy
 	}
 	return &pb.GetOrdersByUserIdResponse{
 		Orders: h.mapOrderForListToResponse(orders),
+	}, nil
+}
+
+func (h *GRPCHandler) CancelOrder(ctx context.Context, req *types.OrderId) (*types.OrderId, error) {
+	userID, err := h.getUserIDFromMetadata(ctx)
+	if err != nil {
+		return nil, err
+	}
+	orderID, err := uuid.Parse(req.Id)
+	if err != nil {
+		return nil, err
+	}
+	orderID, err = h.orderUC.Cancel(orderID, userID)
+	if err != nil {
+		return nil, err
+	}
+	return &types.OrderId{
+		Id: orderID.String(),
 	}, nil
 }
 
