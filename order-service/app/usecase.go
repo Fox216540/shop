5bc6a69d56d963f3b9aead0aa38645b01ddeb876
Andrey <snick25rs@gmail.com>
2025-10-22 02:21:10 +0200

    Переименовал методы Place, Cancel и GetByID в PlaceOrder, CancelOrder и GetOrderByIDAndUserID для большей ясности и специфичности в use case.

--- order-service/app/usecase.go
+++ order-service/app/usecase.go
@@ -7,8 +7,8 @@ import (
 )
 
 type UseCase interface {
-	Place(userID uuid.UUID, orderItems []dto.OrderItems) (order.Order, error)
-	Cancel(ID, userID uuid.UUID) (orderID uuid.UUID, err error)
-	GetByID(ID uuid.UUID) (order.Order, error)
+	PlaceOrder(userID uuid.UUID, orderItems []dto.OrderItems) (order.Order, error)
+	CancelOrder(ID, userID uuid.UUID) (orderID uuid.UUID, err error)
+	GetOrderByIDAndUserID(ID, userID uuid.UUID) (order.Order, error)
 	GetOrdersByUserID(userID uuid.UUID) (orders []order.Order, err error)
 }
