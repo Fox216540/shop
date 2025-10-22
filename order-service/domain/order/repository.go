e1e524879ec68858e23e1255590c5cee19a6a12d
Andrey <snick25rs@gmail.com>
2025-10-22 02:21:34 +0200

    Заменил метод GetByID на GetByIDAndUserID для добавления проверки прав доступа пользователя.

--- order-service/domain/order/repository.go
+++ order-service/domain/order/repository.go
@@ -5,7 +5,7 @@ import "github.com/google/uuid"
 type Repository interface {
 	Save(o Order) (Order, error)
 	Remove(ID, userID uuid.UUID) error
-	GetByID(ID uuid.UUID) (Order, error)
+	GetByIDAndUserID(ID, userID uuid.UUID) (Order, error)
 	CheckOrderNum(orderNum string) error
 	GetOrdersByUserID(userID uuid.UUID) ([]Order, error)
 }
