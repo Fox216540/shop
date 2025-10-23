0c41e4c8b9b503bcad50fcde237cb194ce80ed35
Andrey <snick25rs@gmail.com>
2025-10-23 15:23:23 +0200

    Исправил обработку ошибок в новом файле.

---
+++ user-service/domain/hasher/errors.go
@@ -0,0 +1,41 @@
+package hasher
+
+import "github.com/Fox216540/shop/user-service/core/exception"
+
+const domain = "Hasher"
+
+type DomainBadRequestError struct {
+	*exception.BadRequestError
+}
+
+func (e *DomainBadRequestError) Error() string {
+	return e.BadRequestError.Error()
+}
+
+func (e *DomainBadRequestError) Unwrap() error {
+	return e.BadRequestError
+}
+
+func NewDomainBadRequestError(msg string, err error) *DomainBadRequestError {
+	return &DomainBadRequestError{
+		BadRequestError: exception.NewBadRequestError(msg, domain, err),
+	}
+}
+
+type BadPasswordError struct {
+	*DomainBadRequestError
+}
+
+func (e *BadPasswordError) Error() string {
+	return e.BadRequestError.Error()
+}
+
+func (e *BadPasswordError) Unwrap() error {
+	return e.BadRequestError
+}
+
+func NewBadPasswordError(err error) *BadPasswordError {
+	return &BadPasswordError{
+		DomainBadRequestError: NewDomainBadRequestError("Bad password", err),
+	}
+}
