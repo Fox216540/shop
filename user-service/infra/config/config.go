0473a781633dadc699a5f9b7ffc1bf255d610fba
Andrey <snick25rs@gmail.com>
2025-10-23 15:26:25 +0200

    Добавил файл конфигурации для управления переменными окружения

---
+++ user-service/infra/config/config.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v10"
+)
+
+type Config struct {
+	PostgresHost       string `env:"POSTGRES_HOST,required"`
+	PostgresPort       uint   `env:"POSTGRES_PORT,required"`
+	PostgresUser       string `env:"POSTGRES_USER,required"`
+	PostgresPassword   string `env:"POSTGRES_PASSWORD,required"`
+	PostgresDatabase   string `env:"POSTGRES_DATABASE,required"`
+	ProductServicePort uint   `env:"PRODUCT_SERVICE_PORT,required"`
+	AuthServicePort    uint   `env:"AUTH_SERVICE_PORT,required"`
+	OrderServicePort   uint   `env:"ORDER_SERVICE_PORT,required"`
+}
+
+func LoadConfig() (*Config, error) {
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
