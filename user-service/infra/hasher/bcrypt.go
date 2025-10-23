cd5993c2e04804129f83e82719f177196bbf6230
Andrey <snick25rs@gmail.com>
2025-10-23 15:29:45 +0200

    Переместил код bcrypt из bcrypt.go в bcrypt.go.

--- user-service/infra/hasher/bcrypt.go
+++ user-service/infra/hasher/bcrypt.go
@@ -1,18 +1,18 @@
 package hasher
 
 import (
-	domainHasher "github.com/Fox216540/shop/auth-service/domain/hasher"
+	domainHasher "github.com/Fox216540/shop/user-service/domain/hasher"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type hasher struct {
 }
 
-func NewHasher() domainHasher.Hasher {
+func NewHasher() domainHasher.UseCase {
 	return &hasher{}
 }
 
-func (h *hasher) Hash(password string) (string, error) {
+func (h *hasher) HashPass(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if err != nil {
@@ -22,7 +22,7 @@ func (h *hasher) Hash(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
-func (h *hasher) Verify(password string, hash string) error {
+func (h *hasher) VerifyPass(password string, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	if err != nil {
 		return domainHasher.NewBadPasswordError(err)
