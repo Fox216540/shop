6776345df67495753344025342820d91ff6111fb
Andrey <snick25rs@gmail.com>
2025-10-23 15:28:59 +0200

    Добавил инициализацию и закрытие подключения к базе данных PostgreSQL

---
+++ user-service/infra/db/postgres.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"fmt"
+	"github.com/Fox216540/shop/user-service/infra/config"
+	"github.com/Fox216540/shop/user-service/infra/db/core"
+	mig "github.com/Fox216540/shop/user-service/infra/db/migration"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+// Init инициализирует глобальный объект Database
+func InitPostgres(conf *config.Config) *gorm.DB {
+	user := conf.PostgresUser
+	password := conf.PostgresPassword
+	host := conf.PostgresHost
+	port := conf.PostgresPort
+	databaseName := conf.PostgresDatabase
+	fmt.Println(user, password, host, port, databaseName)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, databaseName, port,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	err = mig.Migration(db)
+	if err != nil {
+		log.Fatalf("failed to run migrations: %v", err)
+	}
+	return db
+}
+
+// Close завершает подключение к базе
+func ClosePostgres(database *core.Database) {
+	if database == nil || database.DB == nil {
+		return
+	}
+	sqlDB, err := database.DB.DB()
+	if err != nil {
+		log.Printf("failed to get raw DB: %v", err)
+		return
+	}
+	sqlDB.Close()
+}
