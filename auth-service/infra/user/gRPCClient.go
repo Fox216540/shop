476fc5260fbf24d7e240441fc25b0115e8584e3d
Andrey <snick25rs@gmail.com>
2025-10-29 19:48:26 +0100

    Добавлен gRPC-клиент для проверки учетных данных пользователя

---
+++ auth-service/infra/user/gRPCClient.go
@@ -0,0 +1,30 @@
+package user
+
+import (
+	"github.com/Fox216540/shop/apigateway-service/infra/client"
+	types "github.com/Fox216540/shop/proto/common/gen"
+	pb "github.com/Fox216540/shop/proto/user-service/gen/interservice"
+	"github.com/google/uuid"
+)
+
+type GRPCClient struct {
+	client *client.GRPCClient
+	pb     pb.InterserviceServiceClient
+}
+
+func (c *GRPCClient) VerifyCredentialsOfUser(phoneOrEmail, password string) (name string, id uuid.UUID, error error) {
+	ctx := c.client.Context()
+	req := &types.CredentialsRequest{
+		PhoneOrEmail: phoneOrEmail,
+		Password:     password,
+	}
+	resp, err := c.pb.VerifyCredentials(ctx, req)
+	if err != nil {
+		return "", uuid.Nil, err
+	}
+	userID, err := uuid.Parse(resp.Id)
+	if err != nil {
+		return "", uuid.Nil, err
+	}
+	return resp.Name, userID, nil
+}
