0d8655ea644eb81e1fe26153a100a04a71f5c306
Andrey <snick25rs@gmail.com>
2025-10-29 19:50:36 +0100

    Переименован метод LogIn в LogInUser для большей ясности и согласованности с другими методами

--- apigateway-service/domain/user/client.go
+++ apigateway-service/domain/user/client.go
@@ -4,5 +4,5 @@ import "github.com/Fox216540/shop/apigateway-service/domain/auth"
 
 type Client interface {
 	RegisterUser(user User) (name string, tokens auth.Tokens, message string, err error)
-	LogIn(phoneOrEmail, password string) (name string, tokens auth.Tokens, message string, err error)
+	LogInUser(phoneOrEmail, password string) (name string, tokens auth.Tokens, message string, err error)
 }
