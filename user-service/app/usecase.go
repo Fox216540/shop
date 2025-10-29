ea6792c6ed5148553e406798346093e498fdf8d2
Andrey <snick25rs@gmail.com>
2025-10-29 19:37:05 +0100

    Изменил метод Login на VerifyCredentials для улучшения ясности и точности

--- user-service/app/usecase.go
+++ user-service/app/usecase.go
@@ -9,7 +9,7 @@ import (
 
 type UseCase interface {
 	Register(user user.User) (user.User, auth.Tokens, error)
-	Login(usernameOrEmail, password string) (user.User, auth.Tokens, error)
+	VerifyCredentials(usernameOrEmail, password string) (name string, id uuid.UUID, err error)
 	UpdateEmail(userID uuid.UUID, newEmail string) (user.User, error)
 	UpdatePassword(userID uuid.UUID, newPassword string) (user.User, error)
 	UpdatePhone(userID uuid.UUID, newPhone string) (user.User, error)
