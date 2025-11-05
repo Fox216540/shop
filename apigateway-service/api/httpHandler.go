e7a86208a835fcd5cff7c90fb18efd28c63afe93
Andrey <snick25rs@gmail.com>
2025-11-05 23:44:13 +0100

    Обновлены обработчики HTTP: изменены сигнатуры методов для использования контекста gin и улучшения обработки ошибок

--- apigateway-service/api/httpHandler.go
+++ apigateway-service/api/httpHandler.go
@@ -2,94 +2,147 @@ package api
 
 import (
 	shopApiGen "github.com/Fox216540/shop/api/gen"
+	aUseCase "github.com/Fox216540/shop/apigateway-service/app/auth"
+	cUseCase "github.com/Fox216540/shop/apigateway-service/app/catalog"
+	uUseCase "github.com/Fox216540/shop/apigateway-service/app/user"
+	"github.com/gin-gonic/gin"
 	openapiTypes "github.com/oapi-codegen/runtime/types"
 	"net/http"
 )
 
 type HTTPHandler struct {
+	authUseCase    aUseCase.UseCase
+	catalogUseCase cUseCase.UseCase
+	userUseCase    uUseCase.UseCase
 }
 
-func (h *HTTPHandler) PostAuthLogin(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) PostAuthLogin(c *gin.Context) {
+	var req shopApiGen.PostAuthLoginJSONRequestBody
+	if err := c.ShouldBindJSON(&req); err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	name, tokens, message, err := h.authUseCase.LogIn(req.PhoneOrEmail, req.Password)
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	resp := shopApiGen.UserWithTokenResponse{
+		AccessToken: tokens.AccessToken,
+		Message:     message,
+		Name:        name,
+	}
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *HTTPHandler) PostAuthLogout(c *gin.Context) {
+	refresh, err := c.Cookie("refresh")
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Decode refresh token"})
+	}
+	msg, err := h.authUseCase.LogOut(refresh)
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": msg})
+}
+
+func (h *HTTPHandler) PostAuthLogoutAll(c *gin.Context) {
+	refresh, err := c.Cookie("refresh")
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Decode refresh token"})
+	}
+	msg, err := h.authUseCase.LogOutAll(refresh)
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": msg})
+}
+
+func (h *HTTPHandler) PostAuthRefresh(c *gin.Context) {
+	refresh, err := c.Cookie("refresh")
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Decode refresh token"})
+	}
+	msg, err := h.authUseCase.RefreshTokens(refresh)
+	if err != nil {
+		//TODO: Придумать ошибку
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": msg})
+}
+
+func (h *HTTPHandler) GetCategories(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) PostAuthLogout(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) GetOrders(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) PostAuthLogoutAll(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) PostOrders(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) PostAuthRefresh(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) DeleteOrdersId(c *gin.Context, id openapiTypes.UUID) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) GetCategories(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) GetOrdersId(c *gin.Context, id openapiTypes.UUID) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) GetOrders(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) GetProducts(c *gin.Context, params shopApiGen.GetProductsParams) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) PostOrders(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) GetProductById(c *gin.Context, id openapiTypes.UUID) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) DeleteOrdersId(w http.ResponseWriter, r *http.Request, id openapiTypes.UUID) {
+func (h *HTTPHandler) CreateUser(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) GetOrdersId(w http.ResponseWriter, r *http.Request, id openapiTypes.UUID) {
+func (h *HTTPHandler) DeleteUser(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) GetProducts(w http.ResponseWriter, r *http.Request, params shopApiGen.GetProductsParams) {
+func (h *HTTPHandler) PatchUsersMeEmail(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) GetProductById(w http.ResponseWriter, r *http.Request, id openapiTypes.UUID) {
+func (h *HTTPHandler) PatchUsersMePassword(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) PatchUsersMePhone(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (h *HTTPHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *HTTPHandler) PatchUsersMeEmail(w http.ResponseWriter, r *http.Request) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *HTTPHandler) PatchUsersMePassword(w http.ResponseWriter, r *http.Request) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *HTTPHandler) PatchUsersMePhone(w http.ResponseWriter, r *http.Request) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (h *HTTPHandler) PatchUsersMeProfile(w http.ResponseWriter, r *http.Request) {
+func (h *HTTPHandler) PatchUsersMeProfile(c *gin.Context) {
 	//TODO implement me
 	panic("implement me")
 }
