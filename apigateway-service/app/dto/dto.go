768dd98609d6f307ed442dd51630c3aceb5151ab
Andrey <snick25rs@gmail.com>
2025-11-05 23:45:12 +0100

    Добавлен DTO для пользователя с полями имени, email, пароля, телефона и адреса

---
+++ apigateway-service/app/dto/dto.go
@@ -0,0 +1,9 @@
+package dto
+
+type User struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Phone    string `json:"phone"`
+	Address  string `json:"address"`
+}
