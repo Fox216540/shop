a67f6990cb0606bddf241ff3f9d81f431120e590
Andrey <snick25rs@gmail.com>
2025-11-05 23:44:22 +0100

    Добавлен интерфейс UseCase для управления аутентификацией пользователей

---
+++ apigateway-service/app/auth/usecase.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+	"github.com/Fox216540/shop/apigateway-service/domain/auth"
+	"github.com/google/uuid"
+)
+
+type UseCase interface {
+	LogIn(phoneOrEmail, password string) (name string, tokens auth.Tokens, message string, err error)
+	LogOut(token string) (message string, err error)
+	LogOutAll(token string) (message string, err error)
+	RefreshTokens(token string) (tokens auth.Tokens, err error)
+	DecodeAccessToken(token string) (userID uuid.UUID, err error)
+}
