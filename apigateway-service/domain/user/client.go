5624c282a90190de23bb9da4112115c985b7f282
Andrey <snick25rs@gmail.com>
2025-11-05 23:47:13 +0100

    Удален метод LogInUser из интерфейса Client

--- apigateway-service/domain/user/client.go
+++ apigateway-service/domain/user/client.go
@@ -4,5 +4,4 @@ import "github.com/Fox216540/shop/apigateway-service/domain/auth"
 
 type Client interface {
 	RegisterUser(user User) (name string, tokens auth.Tokens, message string, err error)
-	LogInUser(phoneOrEmail, password string) (name string, tokens auth.Tokens, message string, err error)
 }
