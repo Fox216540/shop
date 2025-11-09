c252bdbab0ca5d4e69870fdb441c14a65e7e9f56
Andrey <snick25rs@gmail.com>
2025-11-09 01:39:30 +0100

    Добавил метод DeleteUser в интерфейс UseCase для удаления пользователя по ID

--- apigateway-service/app/user/usecase.go
+++ apigateway-service/app/user/usecase.go
@@ -3,8 +3,10 @@ package user
 import (
 	"github.com/Fox216540/shop/apigateway-service/app/dto"
 	"github.com/Fox216540/shop/apigateway-service/domain/auth"
+	"github.com/google/uuid"
 )
 
 type UseCase interface {
 	RegisterUser(user dto.User) (name string, tokens auth.Tokens, message string, err error)
+	DeleteUser(id uuid.UUID) (message string, err error)
 }
