a0bf11731c21d476b60eb124155b6ab9ab97b536
Andrey <snick25rs@gmail.com>
2025-11-09 01:39:18 +0100

    Добавил метод DeleteUser в интерфейс Client для удаления пользователя

--- apigateway-service/domain/user/client.go
+++ apigateway-service/domain/user/client.go
@@ -1,7 +1,11 @@
 package user
 
-import "github.com/Fox216540/shop/apigateway-service/domain/auth"
+import (
+	"github.com/Fox216540/shop/apigateway-service/domain/auth"
+	"github.com/google/uuid"
+)
 
 type Client interface {
 	RegisterUser(user User) (name string, tokens auth.Tokens, message string, err error)
+	DeleteUser(id uuid.UUID) (message string, err error)
 }
