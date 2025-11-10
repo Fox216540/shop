ac3a8dbce1dbe5666a0bcd1dc86a49b875e3c645
Andrey <snick25rs@gmail.com>
2025-11-10 18:56:43 +0100

    Добавил методы для обновления email, пароля, телефона и профиля пользователя в интерфейс Client

--- apigateway-service/domain/user/client.go
+++ apigateway-service/domain/user/client.go
@@ -8,4 +8,8 @@ import (
 type Client interface {
 	RegisterUser(user User) (name string, tokens auth.Tokens, message string, err error)
 	DeleteUser(id uuid.UUID) (message string, err error)
+	UpdateEmail(id uuid.UUID, email string) (message string, err error)
+	UpdatePassword(id uuid.UUID, password string) (message string, err error)
+	UpdatePhone(id uuid.UUID, phone string) (message string, err error)
+	UpdateProfile(id uuid.UUID, name *string, address *string) (message string, err error)
 }
