befdb81282ab3f33d1d0cf06b3a18aae7ee33788
Andrey <snick25rs@gmail.com>
2025-11-12 03:00:43 +0100

    Добавил методы для обновления email, пароля, телефона и профиля пользователя в интерфейс UseCase

--- apigateway-service/app/user/usecase.go
+++ apigateway-service/app/user/usecase.go
@@ -9,4 +9,8 @@ import (
 type UseCase interface {
 	RegisterUser(user dto.User) (name string, tokens auth.Tokens, message string, err error)
 	DeleteUser(id uuid.UUID) (message string, err error)
+	UpdateEmailOfUser(id uuid.UUID, email string) (message string, err error)
+	UpdatePasswordOfUser(id uuid.UUID, password string) (message string, err error)
+	UpdatePhoneOfUser(id uuid.UUID, phone string) (message string, err error)
+	UpdateProfileOfUser(id uuid.UUID, name *string, address *string) (message string, newName string, err error)
 }
