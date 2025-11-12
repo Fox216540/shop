986b46b5ffd34634c53cd25662599485039eb58c
Andrey <snick25rs@gmail.com>
2025-11-12 20:36:41 +0100

    Добавил интерфейс клиента для управления заказами.

--- apigateway-service/domain/order/client.go
+++ apigateway-service/domain/order/client.go
@@ -1,1 +1,10 @@
 package order
+
+import "github.com/google/uuid"
+
+type Client interface {
+	CreateOrder(userID uuid.UUID, items []ProductRequest) (order Order, err error)
+	GetOrder(userID uuid.UUID, orderID uuid.UUID) (order Order, err error)
+	GetOrders(userID uuid.UUID) (orders []Order, err error)
+	DeleteOrder(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, msg string, status string, err error)
+}
