ea2c412ad3e05b0c224a777e52581109634c8eae
Andrey <snick25rs@gmail.com>
2025-11-12 03:00:28 +0100

    Изменил названия методов в интерфейсе Client для улучшения читаемости и согласованности

--- apigateway-service/domain/user/client.go
+++ apigateway-service/domain/user/client.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Client interface {
-	RegisterUser(user User) (name string, tokens auth.Tokens, message string, err error)
-	DeleteUser(id uuid.UUID) (message string, err error)
+	Register(user User) (name string, tokens auth.Tokens, message string, err error)
+	Delete(id uuid.UUID) (message string, err error)
 	UpdateEmail(id uuid.UUID, email string) (message string, err error)
 	UpdatePassword(id uuid.UUID, password string) (message string, err error)
 	UpdatePhone(id uuid.UUID, phone string) (message string, err error)
-	UpdateProfile(id uuid.UUID, name *string, address *string) (message string, err error)
+	UpdateProfile(id uuid.UUID, name *string, address *string) (message string, newName string, err error)
 }
