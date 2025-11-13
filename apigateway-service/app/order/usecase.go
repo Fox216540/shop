2972b45f0a93b8dcfd6938c4a2c43e32907753db
Andrey <snick25rs@gmail.com>
2025-11-13 01:21:24 +0100

    Импортировал зависимости и определил интерфейс UseCase для работы с заказами.

--- apigateway-service/app/order/usecase.go
+++ apigateway-service/app/order/usecase.go
@@ -1,1 +1,13 @@
 package order
+
+import (
+	"github.com/Fox216540/shop/apigateway-service/domain/order"
+	"github.com/google/uuid"
+)
+
+type UseCase interface {
+	Create(userID uuid.UUID, items []order.ProductRequest) (order.Order, error)
+	Get(userID uuid.UUID, orderID uuid.UUID) (order.Order, error)
+	GetOrders(userID uuid.UUID) ([]order.Order, error)
+	Delete(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, msg string, status string, err error)
+}
