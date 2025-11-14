7c95d62c106eb812532d79cff36ff2225df9a87f
Andrey <snick25rs@gmail.com>
2025-11-14 02:59:55 +0100

    Переименовал методы, чтобы согласовать имена с новым API orderClient.

--- apigateway-service/app/order/usecase.go
+++ apigateway-service/app/order/usecase.go
@@ -6,8 +6,8 @@ import (
 )
 
 type UseCase interface {
-	Create(userID uuid.UUID, items []order.ProductRequest) (order.Order, error)
-	Get(userID uuid.UUID, orderID uuid.UUID) (order.Order, error)
+	CreateOrder(userID uuid.UUID, items []order.ProductRequest) (order.Order, error)
+	GetOrder(userID uuid.UUID, orderID uuid.UUID) (order.OrderWithItems, error)
 	GetOrders(userID uuid.UUID) ([]order.Order, error)
-	Delete(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, msg string, status string, err error)
+	DeleteOrder(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, msg string, status string, err error)
 }
