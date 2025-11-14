e9aae22a37dcc19c216fd663664f78a2429a7380
Andrey <snick25rs@gmail.com>
2025-11-14 02:59:39 +0100

    Переименовал методы, чтобы согласовать имена с новым API orderClient.

--- apigateway-service/app/order/service.go
+++ apigateway-service/app/order/service.go
@@ -13,18 +13,18 @@ func NewService(client order.Client) UseCase {
 	return &service{orderClient: client}
 }
 
-func (s *service) Create(userID uuid.UUID, items []order.ProductRequest) (order.Order, error) {
-	o, err := s.orderClient.CreateOrder(userID, items)
+func (s *service) CreateOrder(userID uuid.UUID, items []order.ProductRequest) (order.Order, error) {
+	o, err := s.orderClient.Create(userID, items)
 	if err != nil {
 		return order.Order{}, err
 	}
 	return o, nil
 }
 
-func (s *service) Get(userID uuid.UUID, orderID uuid.UUID) (order.Order, error) {
-	o, err := s.orderClient.GetOrder(userID, orderID)
+func (s *service) GetOrder(userID uuid.UUID, orderID uuid.UUID) (order.OrderWithItems, error) {
+	o, err := s.orderClient.Get(userID, orderID)
 	if err != nil {
-		return order.Order{}, err
+		return order.OrderWithItems{}, err
 	}
 	return o, nil
 }
@@ -37,8 +37,8 @@ func (s *service) GetOrders(userID uuid.UUID) ([]order.Order, error) {
 	return orders, nil
 }
 
-func (s *service) Delete(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, message string, status string, err error) {
-	id, msg, stat, err := s.orderClient.DeleteOrder(userID, orderID)
+func (s *service) DeleteOrder(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, message string, status string, err error) {
+	id, msg, stat, err := s.orderClient.Delete(userID, orderID)
 	if err != nil {
 		return uuid.Nil, "", "", err
 	}
