b0bca58ae7b78bf9f22989f2c8f6d53f3f763d0c
Andrey <snick25rs@gmail.com>
2025-11-14 03:00:17 +0100

    Переименовал методы `CreateOrder` и `GetOrder` в `Create` и `Get`, а также изменил тип возвращаемого значения `Get` на `OrderWithItems`.

--- apigateway-service/domain/order/client.go
+++ apigateway-service/domain/order/client.go
@@ -3,8 +3,8 @@ package order
 import "github.com/google/uuid"
 
 type Client interface {
-	CreateOrder(userID uuid.UUID, items []ProductRequest) (order Order, err error)
-	GetOrder(userID uuid.UUID, orderID uuid.UUID) (order Order, err error)
+	Create(userID uuid.UUID, items []ProductRequest) (order Order, err error)
+	Get(userID uuid.UUID, orderID uuid.UUID) (order OrderWithItems, err error)
 	GetOrders(userID uuid.UUID) (orders []Order, err error)
-	DeleteOrder(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, msg string, status string, err error)
+	Delete(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, msg string, status string, err error)
 }
