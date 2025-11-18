c5334df84ba3e6e0a7b40f7d62af76b8fdb6e79e
Andrey <snick25rs@gmail.com>
2025-11-18 22:39:34 +0100

    Заменил коды HTTP ответов на коды gRPC и обновил сообщения об ошибках для лучшей совместимости с gRPC.

--- auth-service/core/mapError/map.go
+++ auth-service/core/mapError/map.go
@@ -4,26 +4,27 @@ import (
 	"errors"
 	"github.com/Fox216540/shop/auth-service/core/exception"
 	"github.com/Fox216540/shop/auth-service/core/logger"
-	"net/http"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-func MapError(e error) (int, string) {
+func MapError(e error) error {
 	var be *exception.BadRequestError
 	var ue *exception.UnauthorizedError
 	var ne *exception.NotFoundError
 	switch {
 	case errors.As(e, &be):
 		logger.Log.Error(e.Error())
-		return http.StatusBadRequest, "Bad request"
+		return status.Error(codes.InvalidArgument, NewMessages().InvalidArgument)
 	case errors.As(e, &ue):
 		logger.Log.Error(e.Error())
-		return http.StatusUnauthorized, "Unauthorized"
+		return status.Error(codes.Unauthenticated, NewMessages().Unauthorized)
 	case errors.As(e, &ne):
 		logger.Log.Error(e.Error())
-		return http.StatusNotFound, "Not found"
+		return status.Error(codes.NotFound, NewMessages().NotFound)
 	default:
 		logger.Log.Error("Server Error")
 		logger.ErrorLog.Error(e.Error())
-		return http.StatusInternalServerError, "Server Error"
+		return status.Error(codes.Internal, NewMessages().ServerError)
 	}
 }
