d64dfabe26902c31e134e3980ea81dd5f5df957c
Andrey <snick25rs@gmail.com>
2025-11-18 22:38:46 +0100

    Добавил реализацию ошибок для модуля user, включая обработку неверных учетных данных при верификации.

--- auth-service/infra/user/errors.go
+++ auth-service/infra/user/errors.go
@@ -1,1 +1,41 @@
 package user
+
+import "github.com/Fox216540/shop/auth-service/infra/globalError"
+
+const domain = "User"
+
+type UserServerError struct {
+	*globalError.InfraServerError
+}
+
+func (e *UserServerError) Error() string {
+	return e.InfraServerError.Error()
+}
+
+func (e *UserServerError) Unwrap() error {
+	return e.InfraServerError.Unwrap()
+}
+
+func NewUserServerError(msg string, err error) *UserServerError {
+	return &UserServerError{
+		InfraServerError: globalError.NewInfraServerError(msg, domain, err),
+	}
+}
+
+type InvalidVerifyCredentialsOfUserError struct {
+	*UserServerError
+}
+
+func (e *InvalidVerifyCredentialsOfUserError) Error() string {
+	return e.UserServerError.Error()
+}
+
+func (e *InvalidVerifyCredentialsOfUserError) Unwrap() error {
+	return e.UserServerError.Unwrap()
+}
+
+func NewInvalidVerifyCredentialsOfUserError(err error) *InvalidVerifyCredentialsOfUserError {
+	return &InvalidVerifyCredentialsOfUserError{
+		UserServerError: NewUserServerError("Invalid verify credentials of user", err),
+	}
+}
