dde67a0f3da32ef27a613d275328a9b4830523e9
Andrey <snick25rs@gmail.com>
2025-12-12 01:20:20 +0100

    Упрощена обработка ошибок при получении списка категорий с использованием `mapError`.

--- catalog-service/app/category/service.go
+++ catalog-service/app/category/service.go
@@ -1,8 +1,7 @@
 package category
 
 import (
-	"errors"
-	"github.com/Fox216540/shop/catalog-service/core/exception"
+	"github.com/Fox216540/shop/catalog-service/app/mapError"
 	"github.com/Fox216540/shop/catalog-service/domain/category"
 )
 
@@ -17,15 +16,7 @@ func NewService(r category.Repository) UseCase {
 func (s *service) GetAllCategories() ([]category.Category, error) {
 	categories, err := s.r.FindAll()
 	if err != nil {
-		var domainError *exception.DomainError
-		var serverError *exception.ServerError
-		if errors.As(err, &domainError) {
-			return nil, err
-		}
-		if errors.As(err, &serverError) {
-			return nil, err
-		}
-		return nil, NewInvalidGetCategories(err)
+		return nil, mapError.MapError(err, NewInvalidGetCategories(err))
 	}
 	return categories, nil
 }
