811fca6d7c845171b6954a5e6b9dc2b19a2cd87d
Andrey <snick25rs@gmail.com>
2025-12-12 01:20:04 +0100

    Реализовал функцию для сопоставления ошибок домена и сервера, возвращая их без изменений, иначе возвращаю переданную альтернативную ошибку.

---
+++ catalog-service/app/mapError/message.go
@@ -0,0 +1,18 @@
+package mapError
+
+import (
+	"errors"
+	"github.com/Fox216540/shop/catalog-service/core/exception"
+)
+
+func MapError(err, anotherError error) error {
+	var domainError *exception.DomainError
+	var serverError *exception.ServerError
+	if errors.As(err, &domainError) {
+		return err
+	}
+	if errors.As(err, &serverError) {
+		return err
+	}
+	return anotherError
+}
