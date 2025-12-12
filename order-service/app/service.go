dfa36ef351e28f2cf72af2ccbba7144ee125dd75
Andrey <snick25rs@gmail.com>
2025-12-12 01:12:47 +0100

    Рефакторил код сервиса заказов, внеся следующие изменения: - Заменил импорты `errors`, `fmt` на `github.com/Fox216540/shop/order-service/app/mapError` и `github.com/Fox216540/shop/order-service/domain/catalog`. - Переименовал `productClient` в `catalogClient`. - Удалил неиспользуемый метод `mapError`. - Изменил типы `[]*order.Item` на `[]order.Item` в методах `createNewOrder`, `enrichProducts` и `createOrderItems`. - Изменил типы `*p.Product` на `c.Product` в методе `createOrderItems`. - Добавил проверку на существование номера заказа в `generateOrderNum`. - Внедрил использование `mapError.MapError` для обработки ошибок.

--- order-service/app/service.go
+++ order-service/app/service.go
@@ -1,43 +1,29 @@
 package order
 
 import (
-	"errors"
-	"fmt"
 	"github.com/Fox216540/shop/order-service/app/dto"
-	"github.com/Fox216540/shop/order-service/core/exception"
+	"github.com/Fox216540/shop/order-service/app/mapError"
+	c "github.com/Fox216540/shop/order-service/domain/catalog"
 	"github.com/Fox216540/shop/order-service/domain/idgenerator"
 	"github.com/Fox216540/shop/order-service/domain/order"
-	p "github.com/Fox216540/shop/order-service/domain/product"
 	"github.com/google/uuid"
 )
 
 type service struct {
 	r             order.Repository
 	idGen         idgenerator.Generator
-	productClient p.Client
+	catalogClient c.Client
 }
 
 func NewOrderService(
 	r order.Repository,
 	idGen idgenerator.Generator,
-	productClient p.Client,
+	catalogClient c.Client,
 ) UseCase {
-	return &service{r: r, idGen: idGen, productClient: productClient}
+	return &service{r: r, idGen: idGen, catalogClient: catalogClient}
 }
 
-func (s *service) mapError(err, appServerError error) error {
-	var appError *exception.ServerError
-	var domainError *exception.DomainError
-	if errors.As(err, &domainError) {
-		return err
-	}
-	if errors.As(err, &appError) {
-		return err
-	}
-	return appServerError
-}
-
-func (s *service) createNewOrder(userID uuid.UUID, items []*order.Item) order.Order {
+func (s *service) createNewOrder(userID uuid.UUID, items []order.Item) order.Order {
 	return order.Order{
 		ID:         uuid.New(),
 		UserID:     userID,
@@ -45,28 +31,27 @@ func (s *service) createNewOrder(userID uuid.UUID, items []*order.Item) order.Or
 	}
 }
 
-func (s *service) enrichProducts(items []*order.Item) error {
+func (s *service) enrichProducts(items []order.Item) error {
 	productsOrder := make([]uuid.UUID, len(items))
 	for _, item := range items {
 		productsOrder = append(productsOrder, item.Product.ID)
 	}
 
-	productsDb, err := s.productClient.GetProductsByIDs(productsOrder)
+	productsDb, err := s.catalogClient.GetProductsByIDs(productsOrder)
 	if err != nil {
 		return err
 	}
 
-	productMapDb := make(map[uuid.UUID]*p.Product, len(productsDb))
+	productMapDb := make(map[uuid.UUID]c.Product, len(productsDb))
 	for _, prod := range productsDb {
-		productMapDb[prod.ID] = &prod
+		productMapDb[prod.ID] = prod
 	}
 
 	for _, item := range items {
 		if prod, ok := productMapDb[item.Product.ID]; ok {
 			item.Product = prod
 		} else {
-			// TODO: разобраться какую ошибку возвращать
-			return fmt.Errorf("product not found: %s", item.Product.ID)
+			return order.NewProductOfOrderNotFoundError(nil)
 		}
 	}
 
@@ -78,7 +63,9 @@ func (s *service) generateOrderNum(o order.Order) (order.Order, error) {
 	if err != nil {
 		return order.Order{}, err
 	}
-	// TODO: Проверить не занят ли номер заказа
+	if err = s.r.CheckOrderNum(orderNum); err != nil {
+		return order.Order{}, err
+	}
 	o.OrderNum = orderNum
 
 	return o, nil
@@ -93,15 +80,15 @@ func (s *service) calculateOrderTotal(o order.Order) (order.Order, error) {
 	return o, nil
 }
 
-func (s *service) createOrderItems(orderItems []dto.OrderItems) ([]*order.Item, error) {
-	oI := make([]*order.Item, len(orderItems))
+func (s *service) createOrderItems(orderItems []dto.OrderItems) ([]order.Item, error) {
+	oI := make([]order.Item, len(orderItems))
 	for _, item := range orderItems {
 		id, err := uuid.Parse(item.ProductID)
 		if err != nil {
-			return nil, s.mapError(err, NewInvalidPlace(err))
+			return nil, err
 		}
-		oI = append(oI, &order.Item{
-			Product: &p.Product{
+		oI = append(oI, order.Item{
+			Product: c.Product{
 				ID: id,
 			},
 			Quantity: item.Quantity,
@@ -113,28 +100,28 @@ func (s *service) createOrderItems(orderItems []dto.OrderItems) ([]*order.Item, 
 func (s *service) PlaceOrder(userID uuid.UUID, orderItems []dto.OrderItems) (order.Order, error) {
 	oI, err := s.createOrderItems(orderItems)
 	if err != nil {
-		return order.Order{}, s.mapError(err, NewInvalidPlace(err))
+		return order.Order{}, mapError.MapError(err, NewInvalidPlace(err))
 	}
 
 	o := s.createNewOrder(userID, oI)
 
 	if err = s.enrichProducts(o.OrderItems); err != nil {
-		return o, s.mapError(err, NewInvalidPlace(err))
+		return o, mapError.MapError(err, NewInvalidPlace(err))
 	}
 
 	o, err = s.generateOrderNum(o) // Генерируем уникальный номер заказа
 	if err != nil {
-		return o, s.mapError(err, NewInvalidPlace(err))
+		return o, mapError.MapError(err, NewInvalidPlace(err))
 	}
 
 	o, err = s.calculateOrderTotal(o) // Вычисляем общую сумму заказа
 	if err != nil {
-		return o, s.mapError(err, NewInvalidPlace(err))
+		return o, mapError.MapError(err, NewInvalidPlace(err))
 	}
 
 	o, err = s.r.Save(o)
 	if err != nil {
-		return o, s.mapError(err, NewInvalidPlace(err)) // Возвращаем ошибку, если не удалось сохранить заказ
+		return o, mapError.MapError(err, NewInvalidPlace(err)) // Возвращаем ошибку, если не удалось сохранить заказ
 	}
 
 	return o, nil
@@ -142,7 +129,7 @@ func (s *service) PlaceOrder(userID uuid.UUID, orderItems []dto.OrderItems) (ord
 
 func (s *service) DeleteOrder(ID, userID uuid.UUID) error {
 	if err := s.r.Remove(ID, userID); err != nil {
-		return s.mapError(err, NewInvalidCancel(err)) // Возвращаем ошибку, если не удалось удалить заказ
+		return mapError.MapError(err, NewInvalidCancel(err)) // Возвращаем ошибку, если не удалось удалить заказ
 	}
 	return nil
 }
@@ -150,7 +137,7 @@ func (s *service) DeleteOrder(ID, userID uuid.UUID) error {
 func (s *service) GetOrderByIDAndUserID(id, userID uuid.UUID) (order.Order, error) {
 	o, err := s.r.GetByIDAndUserID(id, userID)
 	if err != nil {
-		return o, s.mapError(err, NewInvalidGetByID(err)) // Возвращаем ошибку, если не удалось найти заказ
+		return o, mapError.MapError(err, NewInvalidGetByID(err)) // Возвращаем ошибку, если не удалось найти заказ
 	}
 	return o, nil
 }
@@ -158,7 +145,7 @@ func (s *service) GetOrderByIDAndUserID(id, userID uuid.UUID) (order.Order, erro
 func (s *service) GetOrdersByUserID(userID uuid.UUID) ([]order.Order, error) {
 	orders, err := s.r.GetOrdersByUserID(userID)
 	if err != nil {
-		return nil, s.mapError(err, NewInvalidGetOrdersByUserID(err)) // Возвращаем ошибку, если не удалось найти заказы пользователя
+		return nil, mapError.MapError(err, NewInvalidGetOrdersByUserID(err)) // Возвращаем ошибку, если не удалось найти заказы пользователя
 	}
 	return orders, nil
 }
