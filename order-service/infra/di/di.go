9e6bfa21a4faaf7034e81f494cca017f160ad211
Andrey <snick25rs@gmail.com>
2025-12-12 01:08:33 +0100

    Я переименовал `productClient` в `catalogClient` и импортировал пакет `catalog`.

--- order-service/infra/di/di.go
+++ order-service/infra/di/di.go
@@ -2,16 +2,16 @@ package di
 
 import (
 	"github.com/Fox216540/shop/order-service/app"
+	c "github.com/Fox216540/shop/order-service/infra/catalog"
 	"github.com/Fox216540/shop/order-service/infra/client"
 	"github.com/Fox216540/shop/order-service/infra/db/core"
 	idgen "github.com/Fox216540/shop/order-service/infra/idgenerator"
 	r "github.com/Fox216540/shop/order-service/infra/order"
-	p "github.com/Fox216540/shop/order-service/infra/product"
 )
 
 func GetOrderService(db *core.Database, client *client.GRPCClient) order.UseCase {
 	repo := r.NewRepository(db)
 	idGen := idgen.NewSonyFlakeGenerator()
-	productClient := p.NewGRPCClient(client)
-	return order.NewOrderService(repo, idGen, productClient)
+	catalogClient := c.NewGRPCClient(client)
+	return order.NewOrderService(repo, idGen, catalogClient)
 }
