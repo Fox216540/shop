1528ff816ebf92c1082f012ffba12c8ee9985c03
Andrey <snick25rs@gmail.com>
2025-12-12 01:19:46 +0100

    Заменил пользовательскую функцию обработки ошибок на использование пакета `mapError`.

--- catalog-service/app/product/service.go
+++ catalog-service/app/product/service.go
@@ -1,8 +1,7 @@
 package product
 
 import (
-	"errors"
-	"github.com/Fox216540/shop/catalog-service/core/exception"
+	"github.com/Fox216540/shop/catalog-service/app/mapError"
 	"github.com/Fox216540/shop/catalog-service/domain/product"
 	"github.com/google/uuid"
 )
@@ -15,22 +14,10 @@ func NewService(r product.Repository) UseCase {
 	return &service{r: r}
 }
 
-func (s *service) mapError(err, appServerError error) error {
-	var domainError *exception.DomainError
-	var serverError *exception.ServerError
-	if errors.As(err, &domainError) {
-		return err
-	}
-	if errors.As(err, &serverError) {
-		return err
-	}
-	return appServerError
-}
-
 func (s *service) GetProducts() ([]product.Product, error) {
 	products, err := s.r.FindAllProducts()
 	if err != nil {
-		return nil, s.mapError(err, NewInvalidGetAll(err)) // Возвращаем ошибку, если не удалось найти продукты
+		return nil, mapError.MapError(err, NewInvalidGetAll(err)) // Возвращаем ошибку, если не удалось найти продукты
 	}
 	return products, nil
 }
@@ -38,7 +25,7 @@ func (s *service) GetProducts() ([]product.Product, error) {
 func (s *service) GetProductsOfCategoryID(ID uuid.UUID) ([]product.Product, error) {
 	products, err := s.r.FindProductsByCategoryID(ID)
 	if err != nil {
-		return nil, s.mapError(err, NewInvalidGetProductsOfCategoryID(err)) // Возвращаем ошибку, если не удалось найти продукты
+		return nil, mapError.MapError(err, NewInvalidGetProductsOfCategoryID(err)) // Возвращаем ошибку, если не удалось найти продукты
 	}
 	return products, nil
 }
@@ -46,7 +33,7 @@ func (s *service) GetProductsOfCategoryID(ID uuid.UUID) ([]product.Product, erro
 func (s *service) GetProductByID(ID uuid.UUID) (product.Product, error) {
 	p, err := s.r.FindProductByID(ID)
 	if err != nil {
-		return p, s.mapError(err, NewInvalidGetProductByID(err)) // Возвращаем ошибку, если не удалось найти продукт
+		return p, mapError.MapError(err, NewInvalidGetProductByID(err)) // Возвращаем ошибку, если не удалось найти продукт
 	}
 	return p, nil
 }
@@ -54,7 +41,7 @@ func (s *service) GetProductByID(ID uuid.UUID) (product.Product, error) {
 func (s *service) GetProductsByIDs(IDs []uuid.UUID) ([]product.Product, error) {
 	products, err := s.r.FindProductsByIDs(IDs)
 	if err != nil {
-		return nil, s.mapError(err, NewInvalidGetProductsByIDs(err)) // Возвращаем ошибку, если не удалось найти продукты
+		return nil, mapError.MapError(err, NewInvalidGetProductsByIDs(err)) // Возвращаем ошибку, если не удалось найти продукты
 	}
 
 	if len(products) != len(IDs) {
