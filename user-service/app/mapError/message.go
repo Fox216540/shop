99d7c89f7be5db42339747cd68ba6e8d83c647cf
Andrey <snick25rs@gmail.com>
2025-12-12 01:05:32 +0100

    Реализовал функцию маппинга ошибок, отдающую приоритет доменным, серверным и gRPC ошибкам.

---
+++ user-service/app/mapError/message.go
@@ -0,0 +1,26 @@
+package mapError
+
+import (
+	"errors"
+	"github.com/Fox216540/shop/user-service/core/exception"
+	"google.golang.org/grpc/status"
+)
+
+func MapError(err, anotherError error) error {
+	var domainError *exception.DomainError
+	var serverError *exception.ServerError
+	if errors.As(err, &domainError) {
+		return err
+	}
+	if errors.As(err, &serverError) {
+		return err
+	}
+
+	// Проверка на gRPC ошибку
+	if _, ok := status.FromError(err); ok {
+		// это grpc-status ошибка
+		return err
+	}
+
+	return anotherError
+}
