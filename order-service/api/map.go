7109c53d65579b526b73bf28882b4cd6d38e1659
Andrey <snick25rs@gmail.com>
2025-12-12 01:13:37 +0100

    Добавил маппер ошибок для перевода внутренних ошибок сервиса заказов в gRPC статус.

---
+++ order-service/api/map.go
@@ -0,0 +1,53 @@
+package api
+
+import (
+	"errors"
+	"github.com/Fox216540/shop/order-service/core/exception"
+	"github.com/Fox216540/shop/order-service/core/logger"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ErrorMapper struct {
+	log      logger.Logger
+	errorLog logger.Logger
+}
+
+func NewErrorMapper(log, errorLog logger.Logger) *ErrorMapper {
+	return &ErrorMapper{log: log, errorLog: errorLog}
+}
+
+func (m *ErrorMapper) MapError(e error) error {
+	var be *exception.BadRequestError
+	var ue *exception.UnauthorizedError
+	var ne *exception.NotFoundError
+
+	// -------------------------------
+	// 1) gRPC ошибка → прокинуть вверх
+	// -------------------------------
+	if _, ok := status.FromError(e); ok {
+		return e
+	}
+
+	// -------------------------------
+	// 2) Ваши ошибки → маппинг
+	// -------------------------------
+	switch {
+	case errors.As(e, &be):
+		m.log.Error(e.Error())
+		return status.Error(codes.InvalidArgument, NewMessages().InvalidArgument)
+
+	case errors.As(e, &ue):
+		m.log.Error(e.Error())
+		return status.Error(codes.Unauthenticated, NewMessages().Unauthorized)
+
+	case errors.As(e, &ne):
+		m.log.Error(e.Error())
+		return status.Error(codes.NotFound, NewMessages().NotFound)
+
+	default:
+		m.log.Error("Server Error")
+		m.errorLog.Error(e.Error())
+		return status.Error(codes.Internal, NewMessages().ServerError)
+	}
+}
