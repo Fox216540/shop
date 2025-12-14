b3ff00516b746d28d56aae84095467c015398399
Andrey <snick25rs@gmail.com>
2025-12-14 01:45:48 +0100

    Добавил обработку ошибок с помощью `mapError` для операций добавления, удаления и массового удаления токенов.

--- auth-service/app/tokenstorage/service.go
+++ auth-service/app/tokenstorage/service.go
@@ -1,6 +1,7 @@
 package tokenstorage
 
 import (
+	"github.com/Fox216540/shop/auth-service/app/mapError"
 	"github.com/Fox216540/shop/auth-service/domain/tokenstorage"
 	"github.com/google/uuid"
 )
@@ -15,21 +16,21 @@ func NewService(ts tokenstorage.Repository) UseCase {
 
 func (s *service) Add(jti, userID uuid.UUID) error {
 	if err := s.ts.Set(jti, userID); err != nil {
-		return err
+		return mapError.MapError(err, NewInvalidAdd(err))
 	}
 	return nil
 }
 
 func (s *service) Delete(jti, userID uuid.UUID) error {
 	if err := s.ts.Delete(jti, userID); err != nil {
-		return err
+		return mapError.MapError(err, NewInvalidDelete(err))
 	}
 	return nil
 }
 
 func (s *service) DeleteAll(userID uuid.UUID) error {
 	if err := s.ts.DeleteAll(userID); err != nil {
-		return err
+		return mapError.MapError(err, NewInvalidDeleteAll(err))
 	}
 	return nil
 }
