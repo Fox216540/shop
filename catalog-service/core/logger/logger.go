8d3553282dfbad970762d564dda92fcc900eaacb
Andrey <snick25rs@gmail.com>
2025-12-19 02:51:29 +0100

    Обновил интерфейс Logger для передачи контекста и ошибок.

--- catalog-service/core/logger/logger.go
+++ catalog-service/core/logger/logger.go
@@ -1,7 +1,9 @@
 package logger
 
+import "context"
+
 type Logger interface {
-	Info(msg string)
-	Warn(msg string)
-	Error(msg string)
+	Info(ctx context.Context, msg string)
+	Warn(ctx context.Context, msg string)
+	Error(ctx context.Context, err error)
 }
