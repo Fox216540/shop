d7f669dea7bbd75c15acf8ab533108057493ad85
Andrey <snick25rs@gmail.com>
2025-12-30 02:18:16 +0100

    Добавил интерфейс логгера с методами для записи информационных сообщений, предупреждений и ошибок.

---
+++ payment-service/core/logger/logger.go
@@ -0,0 +1,9 @@
+package logger
+
+import "context"
+
+type Logger interface {
+	Info(ctx context.Context, msg string)
+	Warn(ctx context.Context, msg string)
+	Error(ctx context.Context, err error)
+}
