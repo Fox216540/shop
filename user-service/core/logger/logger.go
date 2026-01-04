5126b0bb720f63866a56c49e223536ada0079cf7
Andrey <snick25rs@gmail.com>
2026-01-04 16:57:57 +0100

    Изменил Panic на Fatal для улучшения обработки ошибок в логере.

--- user-service/core/logger/logger.go
+++ user-service/core/logger/logger.go
@@ -6,5 +6,5 @@ type Logger interface {
 	Info(ctx context.Context, msg string)
 	Warn(ctx context.Context, msg string)
 	Error(ctx context.Context, err error)
-	Panic(ctx context.Context, r interface{})
+	Fatal(ctx context.Context, err error)
 }
