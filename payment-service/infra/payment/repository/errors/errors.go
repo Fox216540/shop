69f58adc540a8553904a8cd01b02eaf51a31521a
Andrey <snick25rs@gmail.com>
2026-01-04 17:08:39 +0100

    Добавлена структура ошибок для сервиса оплаты, включающая специфичные ошибки сохранения платежей.

---
+++ payment-service/infra/payment/repository/errors/errors.go
@@ -0,0 +1,43 @@
+package errors
+
+import (
+	"github.com/Fox216540/shop/payment-service/infra/globalError"
+)
+
+const domain = "Payment | Repo"
+
+type PaymentServerError struct {
+	*globalError.InfraServerError
+}
+
+func (e *PaymentServerError) Error() string {
+	return e.InfraServerError.Error()
+}
+
+func (e *PaymentServerError) Unwrap() error {
+	return e.InfraServerError
+}
+
+func NewPaymentServerError(msg string, err error) *PaymentServerError {
+	return &PaymentServerError{
+		InfraServerError: globalError.NewInfraServerError(msg, domain, err),
+	}
+}
+
+type InvalidSavePayment struct {
+	*PaymentServerError
+}
+
+func (e *InvalidSavePayment) Error() string {
+	return e.PaymentServerError.Error()
+}
+
+func (e *InvalidSavePayment) Unwrap() error {
+	return e.PaymentServerError
+}
+
+func NewInvalidSavePayment(err error) error {
+	return &InvalidSavePayment{
+		PaymentServerError: NewPaymentServerError("Invalid Save payment", err),
+	}
+}
