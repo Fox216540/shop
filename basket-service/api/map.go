37fb47316e82a6d631f8a872a3b1d4473b795da7
Andrei <snick25rs@gmail.com>
2026-06-10 00:15:42 +0200

    Внедрил компонент для преобразования внутренних ошибок в gRPC-статусы с логированием.

---
+++ basket-service/api/map.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"github.com/Fox216540/shop/basket-service/core/exception"
+	"github.com/Fox216540/shop/basket-service/core/logger"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type ErrorMapper struct {
+	errorLog logger.Logger
+}
+
+func NewErrorMapper(errorLog logger.Logger) *ErrorMapper {
+	return &ErrorMapper{errorLog: errorLog}
+}
+
+func (m *ErrorMapper) MapError(ctx context.Context, e error) error {
+	var nfe *exception.NotFoundError
+	var de *exception.DomainError
+	var se *exception.ServerError
+
+	if _, ok := status.FromError(e); ok {
+		return e
+	}
+
+	var code codes.Code
+	switch {
+	case errors.As(e, &nfe):
+		code = codes.NotFound
+	case errors.As(e, &de):
+		code = codes.InvalidArgument
+	case errors.As(e, &se):
+		m.errorLog.Error(ctx, e)
+		code = codes.Internal
+	default:
+		m.errorLog.Error(ctx, e)
+		code = codes.Internal
+	}
+
+	return status.Error(code, e.Error())
+}
