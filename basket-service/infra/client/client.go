6017131a915cd9ae56735f2fd1071e6582a1f644
Andrei <snick25rs@gmail.com>
2026-06-10 00:24:03 +0200

    Добавил утилиту gRPC клиента для сервиса корзины.

---
+++ basket-service/infra/client/client.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GRPCClient struct {
+	conn *grpc.ClientConn
+}
+
+func NewClient(serverAddr string) *GRPCClient {
+	conn, err := grpc.NewClient(serverAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	return &GRPCClient{conn: conn}
+}
+
+func (c *GRPCClient) Conn() *grpc.ClientConn {
+	return c.conn
+}
+
+func (c *GRPCClient) Context() context.Context {
+	return context.Background()
+}
+
+func (c *GRPCClient) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
+	return nil
+}
