8f6ac567f5360542b3480a254795c030a67df639
Andrei <snick25rs@gmail.com>
2026-06-10 02:49:20 +0200

    Добавил структуру и инициализацию сообщений для API.

---
+++ order-service/api/messages.go
@@ -0,0 +1,25 @@
+package api
+
+type Messages struct {
+	MetadataNotFound         string
+	UserIDMetadataNotFound   string
+	InvalidArgument          string
+	NotFound                 string
+	Unauthorized             string
+	Internal                 string
+	OrderDeletedSuccessfully string
+}
+
+func NewMessages() Messages {
+	return Messages{
+		MetadataNotFound:         "metadata not found",
+		UserIDMetadataNotFound:   "user_id metadata not found",
+		InvalidArgument:          "Invalid argument",
+		NotFound:                 "Not found",
+		Unauthorized:             "Unauthorized",
+		Internal:                 "Internal server error",
+		OrderDeletedSuccessfully: "Order deleted successfully",
+	}
+}
+
+var messages = NewMessages()
