7c532fe7df038fc45660f4b7c9dace15a2af654c
Andrei <snick25rs@gmail.com>
2026-06-10 02:59:31 +0200

    Обновил псевдонимы импортов для пакетов app и catalog в конфигурации внедрения зависимостей.

--- order-service/infra/di/di.go
+++ order-service/infra/di/di.go
@@ -1,8 +1,8 @@
 package di
 
 import (
-	"github.com/Fox216540/shop/order-service/app"
-	c "github.com/Fox216540/shop/order-service/infra/catalog"
+	order "github.com/Fox216540/shop/order-service/app"
+	"github.com/Fox216540/shop/order-service/infra/catalog"
 	"github.com/Fox216540/shop/order-service/infra/client"
 	"github.com/Fox216540/shop/order-service/infra/db/core"
 	idgen "github.com/Fox216540/shop/order-service/infra/idgenerator"
@@ -12,6 +12,6 @@ import (
 func GetOrderService(db *core.Database, client *client.GRPCClient) order.UseCase {
 	repo := r.NewRepository(db)
 	idGen := idgen.NewSonyFlakeGenerator()
-	catalogClient := c.NewGRPCClient(client)
+	catalogClient := catalog.NewGRPCClient(client)
 	return order.NewOrderService(repo, idGen, catalogClient)
 }
