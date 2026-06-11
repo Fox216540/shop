883c49ac45628d3af4340be2a73e648ba2444dc0
Andrei <snick25rs@gmail.com>
2026-06-11 23:46:56 +0200

    Обновил структуру GRPCHandler и добавил валидацию входящих данных.

--- payment-service/api/gRPCHandler.go
+++ payment-service/api/gRPCHandler.go
@@ -2,20 +2,23 @@ package api
 
 import (
 	"context"
-	"github.com/Fox216540/shop/payment-service/app/payment"
+	"github.com/Fox216540/shop/payment-service/app"
 	pbInterservice "github.com/Fox216540/shop/proto/payment-service/gen/interservice"
 	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type GRPCHandler struct {
-	paymentUS payment.UseCase
+	paymentUS app.UseCase
 	pbInterservice.UnimplementedInterserviceServiceServer
 	mapper *ErrorMapper
 }
 
-func NewGRPCHandler(useCase payment.UseCase) *GRPCHandler {
+func NewGRPCHandler(useCase app.UseCase, mapper *ErrorMapper) *GRPCHandler {
 	return &GRPCHandler{
 		paymentUS: useCase,
+		mapper:    mapper,
 	}
 }
 
@@ -23,13 +26,22 @@ func (h *GRPCHandler) CreatePayment(
 	ctx context.Context,
 	req *pbInterservice.CreatePaymentRequest,
 ) (*pbInterservice.CreatePaymentResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, messages.InvalidArgument)
+	}
+
+	if req.OrderId == "" || req.Value == "" || req.Currency == "" || req.ReturnUrl == "" {
+		return nil, status.Error(codes.InvalidArgument, messages.InvalidArgument)
+	}
+
 	orderID, err := uuid.Parse(req.OrderId)
 	if err != nil {
-		return nil, h.mapper.MapError(ctx, err)
+		return nil, status.Error(codes.InvalidArgument, messages.InvalidArgument)
 	}
 	newPayment, err := h.paymentUS.CreatePayment(
 		orderID, req.Value,
 		req.Currency, req.Description,
+		req.ReturnUrl,
 	)
 	if err != nil {
 		return nil, h.mapper.MapError(ctx, err)
