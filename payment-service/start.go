89e66af79bfb593366edcae18c0c8b6febc95acf
Andrei <snick25rs@gmail.com>
2026-06-11 23:57:51 +0200

    Реализовал логику запуска и настройки gRPC-сервера для сервиса оплаты.

--- payment-service/start.go
+++ payment-service/start.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/Fox216540/shop/payment-service/api"
+	"github.com/Fox216540/shop/payment-service/infra/config"
+	"github.com/Fox216540/shop/payment-service/infra/db"
+	dbcore "github.com/Fox216540/shop/payment-service/infra/db/core"
+	"github.com/Fox216540/shop/payment-service/infra/di"
+	"github.com/Fox216540/shop/payment-service/infra/logger"
+	"github.com/Fox216540/shop/payment-service/infra/payment/yokassa"
+	pbInterservice "github.com/Fox216540/shop/proto/payment-service/gen/interservice"
+	yo "github.com/rvinnie/yookassa-sdk-go/yookassa"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	logger.InitLogger()
+
+	ctx := context.Background()
+	logg := logger.NewZeroLogger()
+
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbConn, err := db.InitPostgres(ctx, logg, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	database := dbcore.NewDatabase(dbConn)
+	defer db.ClosePostgres(ctx, logg, database)
+
+	client := yo.NewClient(conf.YoKassaAccountID, conf.YoKassaSecretKey)
+	paymentHandler := yo.NewPaymentHandler(client)
+	provider := yokassa.NewProvider(paymentHandler)
+
+	handler := api.NewGRPCHandler(
+		di.NewPaymentModule(database, provider),
+		api.NewErrorMapper(logg),
+	)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", conf.PaymentServicePort))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := grpc.NewServer()
+	pbInterservice.RegisterInterserviceServiceServer(server, handler)
+
+	log.Printf("payment-service listening on %s", lis.Addr().String())
+	if err := server.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
