4a52f7f9d69c640deca9936e7e9dbcd058e23c0a
Andrei <snick25rs@gmail.com>
2026-06-11 23:54:11 +0200

    Рефакторил структуру модуля оплаты и обновил зависимости в DI-контейнере.

--- payment-service/infra/di/di.go
+++ payment-service/infra/di/di.go
@@ -1,18 +1,13 @@
 package di
 
 import (
-	"github.com/Fox216540/shop/payment-service/app/payment"
+	"github.com/Fox216540/shop/payment-service/app"
+	"github.com/Fox216540/shop/payment-service/domain/payment"
 	db "github.com/Fox216540/shop/payment-service/infra/db/core"
 	paymentrepo "github.com/Fox216540/shop/payment-service/infra/payment/repository"
-	"github.com/Fox216540/shop/payment-service/infra/payment/yokassa"
 )
 
-func NewPaymentModule(core *db.Database) payment.UseCase {
+func NewPaymentModule(core *db.Database, provider payment.Provider) app.UseCase {
 	repo := paymentrepo.NewRepository(core)
-	provYo := yokassa.NewProvider()
-	testprovYo := yokassa.NewProvider()
-	providers := payment.NewProviderRouter(provYo, testprovYo)
-	facade := payment.NewFacade(repo, providers)
-
-	return facade
+	return app.NewService(repo, provider)
 }
