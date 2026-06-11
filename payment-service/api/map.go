087af35e07b615dbb166455ee54b1c227bb92d08
Andrei <snick25rs@gmail.com>
2026-06-11 23:47:11 +0200

    Добавил обработку ошибок ConflictError и ServerError в маппинг API.

--- payment-service/api/map.go
+++ payment-service/api/map.go
@@ -21,28 +21,26 @@ func (m *ErrorMapper) MapError(ctx context.Context, e error) error {
 	var bre *exception.BadRequestError
 	var ue *exception.UnauthorizedError
 	var nfe *exception.NotFoundError
+	var ce *exception.ConflictError
+	var se *exception.ServerError
 
-	// -------------------------------
-	// 1) gRPC ошибка → прокинуть вверх
-	// -------------------------------
 	if _, ok := status.FromError(e); ok {
 		return e
 	}
 
-	// -------------------------------
-	// 2) Ваши ошибки → маппинг
-	// -------------------------------
 	var code codes.Code
 	switch {
 	case errors.As(e, &bre):
 		code = codes.InvalidArgument
-
 	case errors.As(e, &ue):
 		code = codes.Unauthenticated
-
 	case errors.As(e, &nfe):
 		code = codes.NotFound
-
+	case errors.As(e, &ce):
+		code = codes.FailedPrecondition
+	case errors.As(e, &se):
+		m.errorLog.Error(ctx, e)
+		code = codes.Internal
 	default:
 		m.errorLog.Error(ctx, e)
 		code = codes.Internal
