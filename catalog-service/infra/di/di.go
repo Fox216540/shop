88906e5f93fb5ac1176cd9bdfe5f19a17e7c5890
Andrei <snick25rs@gmail.com>
2026-06-11 02:10:46 +0200

    Объединил сервисы категорий и продуктов в единый сервис каталога.

--- catalog-service/infra/di/di.go
+++ catalog-service/infra/di/di.go
@@ -1,19 +1,14 @@
 package di
 
 import (
-	"github.com/Fox216540/shop/catalog-service/app/category"
-	"github.com/Fox216540/shop/catalog-service/app/product"
+	"github.com/Fox216540/shop/catalog-service/app"
 	rCategory "github.com/Fox216540/shop/catalog-service/infra/category"
 	"github.com/Fox216540/shop/catalog-service/infra/db/core"
 	rProduct "github.com/Fox216540/shop/catalog-service/infra/product"
 )
 
-func GetProductService(database *core.Database) product.UseCase {
-	repo := rProduct.NewRepository(database)
-	return product.NewService(repo)
-}
-
-func GetCategoryService(database *core.Database) category.UseCase {
-	repo := rCategory.NewRepository(database)
-	return category.NewService(repo)
+func GetCatalogService(database *core.Database) app.UseCase {
+	categoryRepo := rCategory.NewRepository(database)
+	productRepo := rProduct.NewRepository(database)
+	return app.NewService(categoryRepo, productRepo)
 }
