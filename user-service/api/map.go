6ca6aba9cc2577a4b5595ab690bf08ab9c3a25bd
Andrei <snick25rs@gmail.com>
2026-06-11 17:25:22 +0200

    Добавил обработку серверных ошибок с логированием в маппер API.

--- user-service/api/map.go
+++ user-service/api/map.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 	"errors"
+
 	"github.com/Fox216540/shop/user-service/core/exception"
 	"github.com/Fox216540/shop/user-service/core/logger"
 	"google.golang.org/grpc/codes"
@@ -21,27 +22,23 @@ func (m *ErrorMapper) MapError(ctx context.Context, e error) error {
 	var bre *exception.BadRequestError
 	var ue *exception.UnauthorizedError
 	var nfe *exception.NotFoundError
+	var se *exception.ServerError
 
-	// -------------------------------
-	// 1) gRPC ошибка → прокинуть вверх
-	// -------------------------------
 	if _, ok := status.FromError(e); ok {
 		return e
 	}
 
-	// -------------------------------
-	// 2) Ваши ошибки → маппинг
-	// -------------------------------
 	var code codes.Code
 	switch {
 	case errors.As(e, &bre):
 		code = codes.InvalidArgument
-
 	case errors.As(e, &ue):
 		code = codes.Unauthenticated
-
 	case errors.As(e, &nfe):
 		code = codes.NotFound
+	case errors.As(e, &se):
+		m.errorLog.Error(ctx, e)
+		code = codes.Internal
 
 	default:
 		m.errorLog.Error(ctx, e)
