34f8dfcac0526989235ca95acac8df20d45122d3
Andrei <snick25rs@gmail.com>
2026-06-11 23:49:37 +0200

    Удалил неактуальные параметры и добавил конфигурацию для интеграции с ЮKassa.

--- payment-service/infra/config/config.go
+++ payment-service/infra/config/config.go
@@ -1,25 +1,16 @@
 package config
 
-import (
-	"github.com/caarlos0/env/v10"
-	"time"
-)
+import "github.com/caarlos0/env/v10"
 
 type Config struct {
-	RedisHost          string        `env:"REDIS_HOST,required"`
-	RedisPort          uint          `env:"REDIS_PORT,required"`
-	RedisPassword      string        `env:"REDIS_PASSWORD,required"`
-	PostgresHost       string        `env:"POSTGRES_HOST,required"`
-	PostgresPort       uint          `env:"POSTGRES_PORT,required"`
-	PostgresUser       string        `env:"POSTGRES_USER,required"`
-	PostgresPassword   string        `env:"POSTGRES_PASSWORD,required"`
-	PostgresDatabase   string        `env:"POSTGRES_DATABASE,required"`
-	AccessTokenSecret  string        `env:"ACCESS_TOKEN_SECRET,required"`
-	AccessTokenTTL     time.Duration `env:"ACCESS_TOKEN_TTL,required"`
-	RefreshTokenSecret string        `env:"REFRESH_TOKEN_SECRET,required"`
-	RefreshTokenTTL    time.Duration `env:"REFRESH_TOKEN_TTL,required"`
-	BufferSeconds      time.Duration `env:"BUFFER_SECONDS,required"`
-	ProductServicePort uint          `env:"PRODUCT_SERVICE_PORT,required"`
+	PostgresHost       string `env:"POSTGRES_HOST,required"`
+	PostgresPort       uint   `env:"POSTGRES_PORT,required"`
+	PostgresUser       string `env:"POSTGRES_USER,required"`
+	PostgresPassword   string `env:"POSTGRES_PASSWORD,required"`
+	PostgresDatabase   string `env:"POSTGRES_DATABASE,required"`
+	PaymentServicePort uint   `env:"PAYMENT_SERVICE_PORT,required"`
+	YoKassaAccountID   string `env:"YOOKASSA_ACCOUNT_ID,required"`
+	YoKassaSecretKey   string `env:"YOOKASSA_SECRET_KEY,required"`
 }
 
 func LoadConfig() (*Config, error) {
