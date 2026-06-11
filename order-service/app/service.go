a41d9550cf84239f0eda8ec3fa7739c9c9f90ec3
Andrei <snick25rs@gmail.com>
2026-06-11 17:34:16 +0200

    Удалил неиспользуемые типы ошибок и отказался от их ручного оборачивания в сервисном слое.

--- order-service/app/service.go
+++ order-service/app/service.go
@@ -2,7 +2,6 @@ package order
 
 import (
 	"github.com/Fox216540/shop/order-service/app/dto"
-	"github.com/Fox216540/shop/order-service/app/mapError"
 	c "github.com/Fox216540/shop/order-service/domain/catalog"
 	"github.com/Fox216540/shop/order-service/domain/idgenerator"
 	"github.com/Fox216540/shop/order-service/domain/order"
@@ -70,7 +69,7 @@ func (s *service) enrichProducts(items []order.Item) error {
 func (s *service) generateOrderNum(o order.Order) (order.Order, error) {
 	orderNum, err := s.idGen.NewID()
 	if err != nil {
-		return order.Order{}, NewInvalidGenerateOrderNum(err)
+		return order.Order{}, err
 	}
 	if err = s.r.CheckOrderNum(orderNum); err != nil {
 		return order.Order{}, err
@@ -133,28 +132,28 @@ func (s *service) createOrderItems(orderItems []dto.OrderItems) ([]order.Item, e
 func (s *service) PlaceOrder(userID uuid.UUID, orderItems []dto.OrderItems) (order.Order, error) {
 	oI, err := s.createOrderItems(orderItems)
 	if err != nil {
-		return order.Order{}, mapError.MapError(err, NewInvalidPlace(err))
+		return order.Order{}, err
 	}
 
 	o := s.createNewOrder(userID, oI)
 
 	if err = s.enrichProducts(o.OrderItems); err != nil {
-		return o, mapError.MapError(err, NewInvalidPlace(err))
+		return o, err
 	}
 
 	o, err = s.generateOrderNum(o) // Генерируем уникальный номер заказа
 	if err != nil {
-		return o, mapError.MapError(err, NewInvalidPlace(err))
+		return o, err
 	}
 
 	o, err = s.calculateOrderTotal(o) // Вычисляем общую сумму заказа
 	if err != nil {
-		return o, mapError.MapError(err, NewInvalidPlace(err))
+		return o, err
 	}
 
 	o, err = s.r.Save(o)
 	if err != nil {
-		return o, mapError.MapError(err, NewInvalidPlace(err)) // Возвращаем ошибку, если не удалось сохранить заказ
+		return o, err
 	}
 
 	return o, nil
@@ -162,7 +161,7 @@ func (s *service) PlaceOrder(userID uuid.UUID, orderItems []dto.OrderItems) (ord
 
 func (s *service) DeleteOrder(ID, userID uuid.UUID) error {
 	if err := s.r.Remove(ID, userID); err != nil {
-		return mapError.MapError(err, NewInvalidCancel(err))
+		return err
 	}
 	return nil
 }
@@ -170,7 +169,7 @@ func (s *service) DeleteOrder(ID, userID uuid.UUID) error {
 func (s *service) GetOrderByIDAndUserID(id, userID uuid.UUID) (order.Order, error) {
 	o, err := s.r.GetByIDAndUserID(id, userID)
 	if err != nil {
-		return o, mapError.MapError(err, NewInvalidGetByID(err))
+		return o, err
 	}
 	return o, nil
 }
@@ -178,7 +177,7 @@ func (s *service) GetOrderByIDAndUserID(id, userID uuid.UUID) (order.Order, erro
 func (s *service) GetOrdersByUserID(userID uuid.UUID) ([]order.Order, error) {
 	orders, err := s.r.GetOrdersByUserID(userID)
 	if err != nil {
-		return nil, mapError.MapError(err, NewInvalidGetOrdersByUserID(err))
+		return nil, err
 	}
 	return orders, nil
 }
