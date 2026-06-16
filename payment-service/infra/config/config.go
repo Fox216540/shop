5071c5eea04adc43fa2cde9b7d38e737b6ee71b3
Andrei <snick25rs@gmail.com>
2026-06-16 15:52:28 +0200

    Добавил настройку режима провайдера платежей и сделал параметры YoKassa необязательными.

--- payment-service/infra/config/config.go
+++ payment-service/infra/config/config.go
@@ -3,14 +3,15 @@ package config
 import "github.com/caarlos0/env/v10"
 
 type Config struct {
-	PostgresHost       string `env:"POSTGRES_HOST,required"`
-	PostgresPort       uint   `env:"POSTGRES_PORT,required"`
-	PostgresUser       string `env:"POSTGRES_USER,required"`
-	PostgresPassword   string `env:"POSTGRES_PASSWORD,required"`
-	PostgresDatabase   string `env:"POSTGRES_DATABASE,required"`
-	PaymentServicePort uint   `env:"PAYMENT_SERVICE_PORT,required"`
-	YoKassaAccountID   string `env:"YOOKASSA_ACCOUNT_ID,required"`
-	YoKassaSecretKey   string `env:"YOOKASSA_SECRET_KEY,required"`
+	PostgresHost        string `env:"POSTGRES_HOST,required"`
+	PostgresPort        uint   `env:"POSTGRES_PORT,required"`
+	PostgresUser        string `env:"POSTGRES_USER,required"`
+	PostgresPassword    string `env:"POSTGRES_PASSWORD,required"`
+	PostgresDatabase    string `env:"POSTGRES_DATABASE,required"`
+	PaymentServicePort  uint   `env:"PAYMENT_SERVICE_PORT,required"`
+	PaymentProviderMode string `env:"PAYMENT_PROVIDER_MODE" envDefault:"mock"`
+	YoKassaAccountID    string `env:"YOOKASSA_ACCOUNT_ID"`
+	YoKassaSecretKey    string `env:"YOOKASSA_SECRET_KEY"`
 }
 
 func LoadConfig() (*Config, error) {
