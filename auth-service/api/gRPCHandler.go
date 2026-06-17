2a01965401057ce45dd514c9822128c7b9ac444a
Andrei <snick25rs@gmail.com>
2026-06-17 15:55:31 +0200

    Оптимизировал структуру GRPCHandler и добавил валидацию входящих запросов.

--- auth-service/api/gRPCHandler.go
+++ auth-service/api/gRPCHandler.go
@@ -2,17 +2,17 @@ package api
 
 import (
 	"context"
-	"github.com/Fox216540/shop/auth-service/app/auth"
-	"github.com/Fox216540/shop/auth-service/app/user"
+	"github.com/Fox216540/shop/auth-service/app"
 	pbApi "github.com/Fox216540/shop/proto/auth-service/gen/api"
 	pbInterservice "github.com/Fox216540/shop/proto/auth-service/gen/interservice"
 	types "github.com/Fox216540/shop/proto/common/gen"
 	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type GRPCHandler struct {
-	auth auth.UseCase
-	user user.UseCase
+	auth app.UseCase
 	pbApi.UnimplementedApiServiceServer
 	pbInterservice.UnimplementedInterserviceServiceServer
 	mapper *ErrorMapper
@@ -22,14 +22,14 @@ func (h *GRPCHandler) LogIn(
 	ctx context.Context,
 	req *types.CredentialsRequest,
 ) (*types.UserWithTokensResponse, error) {
-	name, id, err := h.user.VerifyUser(req.PhoneOrEmail, req.Password)
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
+	name, tokens, err := h.auth.LogIn(req.PhoneOrEmail, req.Password)
 	if err != nil {
 		return nil, h.mapper.MapError(ctx, err)
 	}
-	tokens, err := h.auth.GenerateTokens(id)
-	if err != nil {
-		return nil, h.mapper.MapError(ctx, err)
-	}
+
 	return &types.UserWithTokensResponse{
 		Tokens: &types.TokensResponse{
 			AccessToken:  tokens.AccessToken,
@@ -48,6 +48,9 @@ func (h *GRPCHandler) LogOut(
 	ctx context.Context,
 	req *types.DecodeTokenRequest,
 ) (*types.MessageResponse, error) {
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
 	if err := h.auth.DeleteRefreshToken(req.Token); err != nil {
 		return nil, h.mapper.MapError(ctx, err)
 	}
@@ -60,6 +63,9 @@ func (h *GRPCHandler) LogOutAll(
 	ctx context.Context,
 	req *types.DecodeTokenRequest,
 ) (*types.MessageResponse, error) {
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
 	if err := h.auth.DeleteAllTokensByToken(req.Token); err != nil {
 		return nil, h.mapper.MapError(ctx, err)
 	}
@@ -72,6 +78,9 @@ func (h *GRPCHandler) RefreshTokens(
 	ctx context.Context,
 	req *types.DecodeTokenRequest,
 ) (*types.TokensResponse, error) {
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
 	tokens, err := h.auth.RefreshTokens(req.Token)
 	if err != nil {
 		return nil, h.mapper.MapError(ctx, err)
@@ -86,6 +95,9 @@ func (h *GRPCHandler) DecodeAccessToken(
 	ctx context.Context,
 	req *types.DecodeTokenRequest,
 ) (*types.UserId, error) {
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
 	userJWT, err := h.auth.DecodeAccessToken(req.Token)
 	if err != nil {
 		return nil, h.mapper.MapError(ctx, err)
@@ -99,6 +111,9 @@ func (h *GRPCHandler) GenerateTokens(
 	ctx context.Context,
 	req *types.UserId,
 ) (*types.TokensResponse, error) {
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
 	userID, err := uuid.Parse(req.Id)
 	if err != nil {
 		return nil, h.mapper.MapError(ctx, err)
@@ -117,6 +132,9 @@ func (h *GRPCHandler) DeleteAllRefreshTokens(
 	ctx context.Context,
 	req *types.UserId,
 ) (*types.MessageResponse, error) {
+	if req == nil {
+		return nil, h.mapper.MapError(ctx, status.Error(codes.InvalidArgument, NewMessages().InvalidArgument))
+	}
 	userID, err := uuid.Parse(req.Id)
 	if err != nil {
 		return nil, h.mapper.MapError(ctx, err)
@@ -130,8 +148,9 @@ func (h *GRPCHandler) DeleteAllRefreshTokens(
 	}, nil
 }
 
-func NewGRPCHandler(useCase auth.UseCase) *GRPCHandler {
+func NewGRPCHandler(authUC app.UseCase, mapper *ErrorMapper) *GRPCHandler {
 	return &GRPCHandler{
-		auth: useCase,
+		auth:   authUC,
+		mapper: mapper,
 	}
 }
