5d11236332c58da89a1a208567dd3f9c15b36b2e
Andrei <snick25rs@gmail.com>
2026-06-17 15:56:19 +0200

    Реорганизовал зависимости и обновил конструктор сервиса аутентификации.

--- auth-service/infra/di/di.go
+++ auth-service/infra/di/di.go
@@ -1,20 +1,18 @@
 package di
 
 import (
-	"github.com/Fox216540/shop/auth-service/app/jwt"
-	"github.com/Fox216540/shop/auth-service/app/tokenstorage"
+	"github.com/Fox216540/shop/auth-service/app"
+	"github.com/Fox216540/shop/auth-service/infra/client"
+	"github.com/Fox216540/shop/auth-service/infra/config"
 	infraJwt "github.com/Fox216540/shop/auth-service/infra/jwt"
 	"github.com/Fox216540/shop/auth-service/infra/redis/core"
 	infraTokenStorage "github.com/Fox216540/shop/auth-service/infra/tokenstorage"
-	"time"
+	infraUser "github.com/Fox216540/shop/auth-service/infra/user"
 )
 
-func GetJwtService(ttlRefresh, ttlAccess time.Duration, secretRefresh, secretAccess string) jwt.UseCase {
-	repo := infraJwt.NewService(ttlRefresh, ttlAccess, secretRefresh, secretAccess)
-	return jwt.NewService(repo)
-}
-
-func GetTokenStorageService(rdb *core.Redis, ttlStorage time.Duration) tokenstorage.UseCase {
-	repo := infraTokenStorage.NewRepository(rdb, ttlStorage)
-	return tokenstorage.NewService(repo)
+func GetAuthService(conf *config.Config, rdb *core.Redis, client *client.GRPCClient) app.UseCase {
+	jwtRepo := infraJwt.NewService(conf.AccessTokenTTL, conf.RefreshTokenTTL, conf.AccessTokenSecret, conf.RefreshTokenSecret)
+	tokenStorageRepo := infraTokenStorage.NewRepository(rdb, conf.RefreshTokenTTL)
+	userClient := infraUser.NewGRPCClient(client)
+	return app.NewService(jwtRepo, tokenStorageRepo, userClient)
 }
