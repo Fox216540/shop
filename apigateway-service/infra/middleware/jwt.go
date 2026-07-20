8eb54375ad0281907e81fceb88d3f9a43d0ef413
Andrei <snick25rs@gmail.com>
2026-07-20 22:15:14 +0200

    Рефакторил JWT-мидлвар, добавил метод Security и вынес логику декодирования токена в отдельную функцию.

--- apigateway-service/infra/middleware/jwt.go
+++ apigateway-service/infra/middleware/jwt.go
@@ -1,16 +1,24 @@
 package middleware
 
 import (
+	shopApiGen "github.com/Fox216540/shop/api/gen"
 	"github.com/Fox216540/shop/apigateway-service/app/tokenDecoder"
+	"github.com/Fox216540/shop/apigateway-service/core/transport"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strings"
 )
 
-const ContextUserID = "user_id"
+const ContextUserID = transport.UserIDKey
 
 type JWTMiddleware struct {
 	tokenDecoder tokenDecoder.UseCase
+}
+
+func NewJWTMiddleware(tokenDecoder tokenDecoder.UseCase) *JWTMiddleware {
+	return &JWTMiddleware{
+		tokenDecoder: tokenDecoder,
+	}
 }
 
 func (jwt *JWTMiddleware) unauthorized(c *gin.Context) {
@@ -18,20 +26,37 @@ func (jwt *JWTMiddleware) unauthorized(c *gin.Context) {
 	c.Abort()
 }
 
+func (jwt *JWTMiddleware) Security() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if _, ok := c.Get(shopApiGen.BearerAuthScopes); !ok {
+			c.Next()
+			return
+		}
+
+		jwt.decodeAccessToken(c)
+	}
+}
+
 func (jwt *JWTMiddleware) DecodeAccessToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if !strings.HasPrefix(authHeader, "Bearer ") {
-			jwt.unauthorized(c)
-			return
-		}
-		token := strings.TrimPrefix(authHeader, "Bearer ")
-		id, err := jwt.tokenDecoder.DecodeAccessToken(token)
-		if err != nil {
-			jwt.unauthorized(c)
-			return
-		}
-		c.Set(ContextUserID, id)
-		c.Next()
+		jwt.decodeAccessToken(c)
 	}
+}
+
+func (jwt *JWTMiddleware) decodeAccessToken(c *gin.Context) {
+	authHeader := c.GetHeader(transport.AuthorizationHeader)
+	if !strings.HasPrefix(authHeader, transport.BearerPrefix) {
+		jwt.unauthorized(c)
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, transport.BearerPrefix)
+	id, err := jwt.tokenDecoder.DecodeAccessToken(token)
+	if err != nil {
+		jwt.unauthorized(c)
+		return
+	}
+
+	c.Set(ContextUserID, id)
+	c.Next()
 }
