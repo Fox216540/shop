ada09090eeba8426b0e684eb33ddcacc6cbc0ef5
Andrei <snick25rs@gmail.com>
2026-07-20 22:44:51 +0200

    Добавил структуру для обработки ошибок инфраструктурного слоя.

---
+++ apigateway-service/infra/globalError/errors.go
@@ -0,0 +1,23 @@
+package globalError
+
+import "github.com/Fox216540/shop/apigateway-service/core/exception"
+
+const layer = "Infra"
+
+type InfraServerError struct {
+	*exception.ServerError
+}
+
+func (e *InfraServerError) Error() string {
+	return e.ServerError.Error()
+}
+
+func (e *InfraServerError) Unwrap() error {
+	return e.ServerError
+}
+
+func NewInfraServerError(msg, domain string, err error) *InfraServerError {
+	return &InfraServerError{
+		ServerError: exception.NewServerError(msg, domain, layer, err),
+	}
+}
