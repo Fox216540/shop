49462dd54ed4fc82e849feaa34ba8d1ec1248cf8
Andrei <snick25rs@gmail.com>
2026-07-20 22:44:45 +0200

    Добавил контекст в методы логирования и расширил функционал обработки ошибок.

--- apigateway-service/infra/logger/logger.go
+++ apigateway-service/infra/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"context"
 	"os"
 
 	"github.com/rs/zerolog"
@@ -22,14 +23,18 @@ func NewZeroLogger() *ZeroLogger {
 	}
 }
 
-func (l *ZeroLogger) Info(msg string) {
+func (l *ZeroLogger) Info(ctx context.Context, msg string) {
 	l.log.Info().Msg(msg)
 }
 
-func (l *ZeroLogger) Warn(msg string) {
+func (l *ZeroLogger) Warn(ctx context.Context, msg string) {
 	l.log.Warn().Msg(msg)
 }
 
-func (l *ZeroLogger) Error(msg string) {
-	l.log.Error().Caller().Msg(msg)
+func (l *ZeroLogger) Error(ctx context.Context, err error) {
+	l.log.Error().Caller().Stack().Err(err).Send()
+}
+
+func (l *ZeroLogger) Fatal(ctx context.Context, err error) {
+	l.log.Fatal().Caller().Stack().Err(err).Send()
 }
