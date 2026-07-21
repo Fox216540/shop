739b9e8bb820347dd6e0e187e2acf2f4d6ce2576
Andrei <snick25rs@gmail.com>
2026-07-21 02:45:27 +0200

    Добавил новые сообщения об ошибках и константы для работы с токенами.

--- apigateway-service/api/messages.go
+++ apigateway-service/api/messages.go
@@ -6,8 +6,14 @@ type Messages struct {
 	LogOutAllSuccess              string
 	DeleteAllRefreshTokensSuccess string
 	InvalidArgument               string
+	InvalidJSON                   string
 	NotFound                      string
 	Unauthorized                  string
+	MissingRefreshCookie          string
+	MissingUserID                 string
+	RefreshCookieName             string
+	RefreshSuccess                string
+	ServiceUnavailable            string
 	ServerError                   string
 }
 
@@ -18,8 +24,16 @@ func NewMessages() Messages {
 		LogOutAllSuccess:              "Log out all success",
 		DeleteAllRefreshTokensSuccess: "Delete all refresh tokens success",
 		InvalidArgument:               "Invalid argument",
+		InvalidJSON:                   "Invalid JSON payload",
 		NotFound:                      "Not found",
 		Unauthorized:                  "Unauthorized",
+		MissingRefreshCookie:          "Missing refresh cookie",
+		MissingUserID:                 "User id not found",
+		RefreshCookieName:             "__Host-refresh",
+		RefreshSuccess:                "Access and refresh tokens updated successfully",
+		ServiceUnavailable:            "Service unavailable",
 		ServerError:                   "Server error",
 	}
 }
+
+var messages = NewMessages()
