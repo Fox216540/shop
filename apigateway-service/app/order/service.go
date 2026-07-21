a8338f867e86d7816f46572dc23b23dfadbfc07b
Andrei <snick25rs@gmail.com>
2026-07-21 02:44:38 +0200

    Упростил методы сервиса заказов за счет удаления лишней обработки ошибок.

--- apigateway-service/app/order/service.go
+++ apigateway-service/app/order/service.go
@@ -14,33 +14,17 @@ func NewService(client order.Client) UseCase {
 }
 
 func (s *service) CreateOrder(userID uuid.UUID, items []order.ProductRequest) (order.Order, error) {
-	o, err := s.orderClient.Create(userID, items)
-	if err != nil {
-		return order.Order{}, err
-	}
-	return o, nil
+	return s.orderClient.Create(userID, items)
 }
 
 func (s *service) GetOrder(userID uuid.UUID, orderID uuid.UUID) (order.OrderWithItems, error) {
-	o, err := s.orderClient.Get(userID, orderID)
-	if err != nil {
-		return order.OrderWithItems{}, err
-	}
-	return o, nil
+	return s.orderClient.Get(userID, orderID)
 }
 
 func (s *service) GetOrders(userID uuid.UUID) ([]order.Order, error) {
-	orders, err := s.orderClient.GetOrders(userID)
-	if err != nil {
-		return nil, err
-	}
-	return orders, nil
+	return s.orderClient.GetOrders(userID)
 }
 
 func (s *service) DeleteOrder(userID uuid.UUID, orderID uuid.UUID) (ID uuid.UUID, message string, status string, err error) {
-	id, msg, stat, err := s.orderClient.Delete(userID, orderID)
-	if err != nil {
-		return uuid.Nil, "", "", err
-	}
-	return id, msg, stat, nil
+	return s.orderClient.Delete(userID, orderID)
 }
